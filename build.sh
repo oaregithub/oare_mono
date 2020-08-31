9eed5235e38c70b18fab3a5352e3685f22a49993
Brett Fisher <brett.fisher988@gmail.com>
2020-08-31 15:58:04 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@ yarn build
 cd ../backend
 rm -rf dist
 cp -r ../frontend/dist .
+rm -rf ../frontend/dist
 yarn build
-git archive --format=zip --output=oare_node.zip HEAD
+cd ../..
+stashName=`git stash create`
+git archive --format=zip --output=oare_node.zip $stashName ./packages/backend
+rm -rf ./packages/backend/build
+rm -rf ./packages/backend/dist
