c70eb91fe26a060d556fb91a1421d3ba7b737c41
Brett Fisher <brett.fisher988@gmail.com>
2020-09-02 14:38:25 -0600

    Update build:q

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-cd packages/frontend
+cd packages/oare
+yarn build
+cd ../frontend
 yarn build
 cd ../backend
 rm -rf dist
 cp -r ../frontend/dist .
 rm -rf ../frontend/dist
 yarn build
-zip -r oare_build.zip build dist package.json yarn.lock
-mv oare_build.zip ../..
+# zip -r oare_build.zip build dist package.json yarn.lock
+# mv oare_build.zip ../..
