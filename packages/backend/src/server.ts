3321e1d150ea63bbff10fb735b28e7cef0c05552
Brett Fisher <brett.fisher988@gmail.com>
2020-09-08 11:44:01 -0600

    Prevent CORS issue inside Docker

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -19,6 +19,28 @@ declare global {
 
 const app = express();
 
+// Prevent CORS issue inside Docker
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    // const origin = req.get('origin');
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma',
+    );
+
+    // intercept OPTIONS
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  });
+}
+
 app.use(express.json());
 app.use(
   express.urlencoded({
