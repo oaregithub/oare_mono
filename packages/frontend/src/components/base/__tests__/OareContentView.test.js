973a345e35047e46e7584b5cda7d4c97c48515fe
Brett Fisher <brett.fisher988@gmail.com>
2020-09-11 11:32:33 -0600

    Upgrade Vue test utils

--- packages/frontend/src/components/base/__tests__/OareContentView.test.js
+++ packages/frontend/src/components/base/__tests__/OareContentView.test.js
@@ -1,42 +1,35 @@
 import Vuetify from "vuetify";
 import VueCompositionApi from "@vue/composition-api";
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
 import OareContentView from "@/components/base/OareContentView.vue";
 
 const localVue = createLocalVue();
 localVue.use(VueCompositionApi);
 
 describe("OareContentView", () => {
-  let wrapper;
   const TITLE = "Content view title";
 
-  beforeEach(() => {
-    wrapper = shallowMount(OareContentView, {
+  const createWrapper = (props = {}) =>
+    mount(OareContentView, {
       localVue,
       vuetify: new Vuetify(),
       propsData: {
         title: TITLE,
+        ...props,
       },
     });
-  });
 
   it("matches snapshot", () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(createWrapper()).toMatchSnapshot();
   });
 
   it("shows title", () => {
-    expect(wrapper.find("v-card-title-stub").text()).toBe(TITLE);
+    expect(createWrapper().find(".test-content-title").text()).toBe(TITLE);
   });
 
   it("shows loading bar when given loading prop", () => {
-    wrapper = shallowMount(OareContentView, {
-      vuetify: new Vuetify(),
-      propsData: {
-        title: TITLE,
-        loading: true,
-      },
-    });
+    let wrapper = createWrapper({ loading: true });
 
-    expect(wrapper.find("v-progress-linear-stub")).not.toBe(null);
+    expect(wrapper.findComponent({ name: "v-progress-linear" })).not.toBe(null);
   });
 });
