172ad90496b782f1667b07ef316b4c7c65871869
hbludworth <harrisonbludworth@gmail.com>
2020-11-27 11:16:14 -0700

    lint fix

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -21,7 +21,7 @@ sl.set('serverProxy', serverProxy);
 sl.set('globalActions', globalActions);
 sl.set('store', tsStore);
 sl.set('lodash', _);
-sl.set('router', router)
+sl.set('router', router);
 
 loadBases();
 
