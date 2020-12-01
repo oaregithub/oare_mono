60e9d0b19cee04952adffbe8bbf8763c65d88b50
hbludworth <harrisonbludworth@gmail.com>
2020-11-30 17:29:26 -0700

    made migration idempotent

--- packages/backend/migrations/20201130124700_public_blacklist.ts
+++ packages/backend/migrations/20201130124700_public_blacklist.ts
@@ -1,11 +1,15 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('public_blacklist', (table) => {
-    table.increments('id');
-    table.uuid('uuid');
-    table.string('type');
-  });
+  const exists = await knex.schema.hasTable('public_blacklist');
+  if (!exists) {
+    return knex.schema.createTable('public_blacklist', (table) => {
+      table.increments('id');
+      table.uuid('uuid');
+      table.string('type');
+    });
+  }
+  return undefined;
 }
 
 export async function down(knex: Knex): Promise<void> {
