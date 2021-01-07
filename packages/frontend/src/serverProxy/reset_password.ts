c375655dcf6c0cf6e4ea0ef14a41e46553d58f4b
bandrewfisher <brett.fisher988@gmail.com>
2021-01-07 12:41:48 -0700

    View that handles resetting password

--- packages/frontend/src/serverProxy/reset_password.ts
+++ packages/frontend/src/serverProxy/reset_password.ts
@@ -1,9 +1,15 @@
 import axios from '@/axiosInstance';
+import { ResetPasswordPayload } from '@oare/types';
 
 async function sendResetPasswordEmail(email: string) {
   await axios.post('/reset_password', { email });
 }
 
+async function resetPassword(payload: ResetPasswordPayload) {
+  await axios.patch('/reset_password', payload);
+}
+
 export default {
   sendResetPasswordEmail,
+  resetPassword,
 };
