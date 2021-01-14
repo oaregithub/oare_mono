3060ceaf8b17e0668dce516b15ad59a8f9b26c23
hbludworth <harrisonbludworth@gmail.com>
2021-01-13 17:21:44 -0700

    fixed bugs

--- packages/backend/src/api/daos/PermissionsDao/index.ts
+++ packages/backend/src/api/daos/PermissionsDao/index.ts
@@ -1,10 +1,11 @@
 import knex from '@/connection';
 import { PermissionResponse, DictionaryPermissionRow, PagesPermissionRow } from '@oare/types';
-import UserGroupDao from '@/api/daos/UserGroupDao';
+import sl from '@/serviceLocator';
 import { UserRow } from '../UserDao';
 
 class PermissionsDao {
   async getUserPermissions(user: UserRow | null): Promise<PermissionResponse> {
+    const UserGroupDao = sl.get('UserGroupDao');
     if (user && user.isAdmin) {
       return {
         dictionary: [
--- packages/backend/src/api/permissions.ts
+++ packages/backend/src/api/permissions.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { HttpInternalError } from '@/exceptions';
-import PermissionsDao from '@/api/daos/PermissionsDao';
+import sl from '@/serviceLocator';
 
 const router = express.Router();
 
 router.route('/permissions').get(async (req, res, next) => {
   try {
     const { user } = req;
+    const PermissionsDao = sl.get('PermissionsDao');
 
     const permissions = await PermissionsDao.getUserPermissions(user);
 
