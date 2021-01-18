fffd5d9367f438a7a5803f397ee6011e3a3274ca
Brett Fisher <brett.fisher988@gmail.com>
2021-01-18 11:07:47 -0700

    Use mailgun

--- packages/backend/src/mailer.ts
+++ packages/backend/src/mailer.ts
@@ -1,21 +1,38 @@
 import * as nodemailer from 'nodemailer';
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.RESET_EMAIL,
-    pass: process.env.RESET_EMAIL_PASSWORD,
-  },
-});
+import mailgun from 'mailgun-js';
 
 const mailer = {
-  sendMail: ({ to, subject, text }: { to: string; subject: string; text: string }) => {
-    transporter.sendMail({
-      from: process.env.RESET_EMAIL,
-      to,
-      subject,
-      text,
-    });
+  sendMail: async ({ to, subject, text }: { to: string; subject: string; text: string }) => {
+    if (process.env.NODE_ENV === 'production' && process.env.MG_API_KEY) {
+      const mg = mailgun({
+        apiKey: process.env.MG_API_KEY,
+        domain: 'oare.byu.edu',
+      });
+
+      const data = {
+        from: 'OARE Support',
+        to,
+        subject,
+        text,
+      };
+
+      await mg.messages().send(data);
+    } else {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.RESET_EMAIL,
+          pass: process.env.RESET_EMAIL_PASSWORD,
+        },
+      });
+
+      await transporter.sendMail({
+        from: process.env.RESET_EMAIL,
+        to,
+        subject,
+        text,
+      });
+    }
   },
 };
 
