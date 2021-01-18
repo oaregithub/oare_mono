41bd0c61f3ca6a5ac1c7482df082875ebd5683f4
Brett Fisher <brett.fisher988@gmail.com>
2021-01-18 11:37:57 -0700

    Test mailgun locally

--- packages/backend/src/mailer.ts
+++ packages/backend/src/mailer.ts
@@ -1,37 +1,25 @@
-import * as nodemailer from 'nodemailer';
 import mailgun from 'mailgun-js';
 
 const mailer = {
   sendMail: async ({ to, subject, text }: { to: string; subject: string; text: string }) => {
-    if (process.env.NODE_ENV === 'production' && process.env.MG_API_KEY) {
+    if (process.env.MG_API_KEY) {
+      console.log('sending mail');
       const mg = mailgun({
         apiKey: process.env.MG_API_KEY,
-        domain: 'oare.byu.edu',
+        domain:
+          process.env.NODE_ENV === 'production'
+            ? 'oare.byu.edu'
+            : 'sandbox3f749aa55c7947f5a26959616a7a0054.mailgun.org',
       });
 
       const data = {
-        from: 'OARE Support',
+        from: 'OARE Support <oarefeedback@gmail.com>',
         to,
         subject,
         text,
       };
 
       await mg.messages().send(data);
-    } else {
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.RESET_EMAIL,
-          pass: process.env.RESET_EMAIL_PASSWORD,
-        },
-      });
-
-      await transporter.sendMail({
-        from: process.env.RESET_EMAIL,
-        to,
-        subject,
-        text,
-      });
     }
   },
 };
