ec30e00846773af4f5507a225f01ad8bbf01c9ba
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-02-02 19:54:30 -0700

    Converted double for loop into a nested Promise.all().

--- packages/backend/src/api/threads.ts
+++ packages/backend/src/api/threads.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import sl from '@/serviceLocator';
 import { HttpInternalError } from '@/exceptions';
-import { CommentDisplay, Thread, ThreadWithComments } from '@oare/types';
+import { CommentDisplay, Thread, ThreadWithComments, Comment } from '@oare/types';
 
 const router = express.Router();
 
@@ -13,34 +13,40 @@ router.route('/threads/:referenceUuid').get(async (req, res, next) => {
     const userDao = sl.get('UserDao');
 
     const threads: Thread[] | null = await threadsDao.getByReferenceUuid(referenceUuid);
-    const results: ThreadWithComments[] = [];
-    let commentDisplays: CommentDisplay[] = [];
+    let results: ThreadWithComments[] = [];
 
-    if (threads) {
-      for (let i = 0; i < threads.length; i += 1) {
-        const threadUuid = threads[i].uuid;
-        if (threadUuid) {
-          const comments = await commentsDao.getAllByThreadUuid(threadUuid);
-          commentDisplays = [];
-          for (let j = 0; j < comments.length; j += 1) {
-            const user = await userDao.getUserByUuid(comments[j].userUuid);
-            commentDisplays.push({
-              uuid: comments[j].uuid,
-              threadUuid: comments[j].threadUuid,
-              userUuid: comments[j].userUuid ? comments[j].userUuid : null,
+    const getUsersByComments = async (comments: Comment[]): Promise<CommentDisplay[]> => {
+      return Promise.all(
+        comments.map((comment: Comment) =>
+          userDao.getUserByUuid(comment.userUuid).then((user) => {
+            return {
+              uuid: comment.uuid,
+              threadUuid: comment.threadUuid,
+              userUuid: comment.userUuid ? comment.userUuid : null,
               userFirstName: user ? user.firstName : '',
               userLastName: user ? user.lastName : '',
-              createdAt: new Date(comments[j].createdAt),
-              deleted: comments[j].deleted,
-              text: comments[j].text,
+              createdAt: new Date(comment.createdAt),
+              deleted: comment.deleted,
+              text: comment.text,
+            } as CommentDisplay;
+          }),
+        ),
+      );
+    };
+
+    if (threads) {
+      results = await Promise.all(
+        threads.map((thread) =>
+          commentsDao.getAllByThreadUuid(thread.uuid || '').then((comments) => {
+            return getUsersByComments(comments).then((commentDisplays) => {
+              return {
+                thread,
+                comments: commentDisplays,
+              } as ThreadWithComments;
             });
-          }
-          results.push({
-            thread: threads[i],
-            comments: commentDisplays,
-          });
-        }
-      }
+          }),
+        ),
+      );
     }
 
     res.json(results);
