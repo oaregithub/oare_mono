a25ac9b7c17b1b16aa028391786b4eba45e92e02
hbludworth <harrisonbludworth@gmail.com>
2021-02-05 16:06:40 -0700

    created error table

---
+++ packages/backend/migrations/20210205155806_create_error_table.ts
@@ -0,0 +1,22 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasTable = knex.schema.hasTable('errors');
+
+  if (!hasTable) {
+    await knex.schema.createTable('errors', (table) => {
+      table.increments('id');
+      table.uuid('user_uuid').nullable();
+      table.text('description').notNullable();
+      table.text('stacktrace').nullable();
+      table.timestamp('timestamp').notNullable();
+      table.string('status');
+
+      table.foreign('user_uuid').references('user.uuid');
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('errors');
+}
