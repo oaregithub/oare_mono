75dabc9115768f8e9bdbf456078a9c208f02a073
hbludworth <harrisonbludworth@gmail.com>
2021-02-09 17:15:58 -0700

    initial commit

--- packages/backend/src/api/errors.ts
+++ packages/backend/src/api/errors.ts
@@ -3,39 +3,55 @@ import { ErrorsRow, ErrorsPayload } from '@oare/types';
 import { HttpInternalError } from '@/exceptions';
 import sl from '@/serviceLocator';
 import { v4 } from 'uuid';
+import adminRoute from '../middlewares/adminRoute';
 
 const router = express.Router();
 
-router.route('/errors').post(async (req, res, next) => {
-  try {
-    const ErrorsDao = sl.get('ErrorsDao');
+router
+  .route('/errors')
+  .post(async (req, res, next) => {
+    try {
+      const ErrorsDao = sl.get('ErrorsDao');
 
-    const uuid = v4();
-    const user = req.user || null;
-    const { description, stacktrace, status }: ErrorsPayload = req.body;
-    const timestamp = new Date();
+      const uuid = v4();
+      const user = req.user || null;
+      const { description, stacktrace, status }: ErrorsPayload = req.body;
+      const timestamp = new Date();
 
-    let userUuid;
-    if (user) {
-      userUuid = user.uuid;
-    } else {
-      userUuid = null;
+      let userUuid;
+      if (user) {
+        userUuid = user.uuid;
+      } else {
+        userUuid = null;
+      }
+
+      const insertRow: ErrorsRow = {
+        uuid,
+        user_uuid: userUuid,
+        description,
+        stacktrace,
+        timestamp,
+        status,
+      };
+
+      await ErrorsDao.logError(insertRow);
+      res.status(201).end();
+    } catch (err) {
+      next(new HttpInternalError(err));
     }
+  })
+  .get(adminRoute, async (req, res, next) => {
+    try {
+      const ErrorsDao = sl.get('ErrorsDao');
+      const utils = sl.get('utils');
 
-    const insertRow: ErrorsRow = {
-      uuid,
-      user_uuid: userUuid,
-      description,
-      stacktrace,
-      timestamp,
-      status,
-    };
+      const { page, limit } = utils.extractPagination(req.query);
 
-    await ErrorsDao.logError(insertRow);
-    res.status(201).end();
-  } catch (err) {
-    next(new HttpInternalError(err));
-  }
-});
+      const response = await ErrorsDao.getErrorLog(page, limit);
+      res.json(response);
+    } catch (err) {
+      next(new HttpInternalError(err));
+    }
+  });
 
 export default router;
--- packages/frontend/src/serverProxy/errors.ts
+++ packages/frontend/src/serverProxy/errors.ts
@@ -1,10 +1,18 @@
-import { ErrorsPayload } from '@oare/types';
+import { ErrorsPayload, Pagination, ErrorsRow } from '@oare/types';
 import axios from '../axiosInstance';
 
 async function logError(payload: ErrorsPayload): Promise<void> {
   await axios.post('/errors', payload);
 }
 
+async function getErrorLog(payload: Pagination): Promise<ErrorsRow[]> {
+  const { data } = await axios.get('/errors', {
+    params: payload,
+  });
+  return data;
+}
+
 export default {
   logError,
+  getErrorLog,
 };
