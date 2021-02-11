2344324d525aad3bc8f8eac3542f9ed8b571dff5
hbludworth <63749412+hbludworth@users.noreply.github.com>
2021-02-10 23:31:35 -0700

    Connected error middleware to error logging (#251)

--- packages/backend/src/middlewares/error.ts
+++ packages/backend/src/middlewares/error.ts
@@ -1,17 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpException from '@/exceptions/HttpException';
+import { InsertErrorsRow } from '../api/daos/ErrorsDao';
+import sl from '../serviceLocator';
 
-function errorMiddleware(error: HttpException, _request: Request, response: Response, _next: NextFunction) {
+const errorMiddleware = async (error: HttpException, request: Request, response: Response, _next: NextFunction) => {
+  const ErrorsDao = sl.get('ErrorsDao');
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
 
   if (process.env.NODE_ENV !== 'test') {
+    const userUuid = request.user ? request.user.uuid : null;
+    const stacktrace = error.stack || null;
+
+    const insertRow: InsertErrorsRow = {
+      userUuid,
+      description: message,
+      stacktrace,
+      status: 'New',
+    };
+
+    await ErrorsDao.logError(insertRow);
     console.log(message);
   }
   response.status(status).send({
     status,
     message,
   });
-}
+};
 
 export default errorMiddleware;
