0fbd202e1009c6f19eae77c49b8b42bd591c2483
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-02-12 10:34:32 -0700

    Make sure that the name column doesn't exist before adding it.

--- packages/backend/migrations/20210212101302_add-name-column-to-threads.ts
+++ packages/backend/migrations/20210212101302_add-name-column-to-threads.ts
@@ -4,9 +4,12 @@ export async function up(knex: Knex): Promise<void> {
   const hasTable = await knex.schema.hasTable('threads');
 
   if (hasTable) {
-    await knex.schema.table('threads', table => {
-      table.string('name');
-    });
+    const hasColumn = await knex.schema.hasColumn('threads', 'name');
+    if (!hasColumn) {
+      await knex.schema.table('threads', table => {
+        table.string('name');
+      });
+    }
   }
 }
 
