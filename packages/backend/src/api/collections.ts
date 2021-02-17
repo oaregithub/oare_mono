a541a65c82e5bad6e254bf0f44fa3fd43fa7f158
bandrewfisher <brett.fisher988@gmail.com>
2021-02-17 14:28:41 -0700

    262 speed up collections (#280)

--- packages/backend/src/api/collections.ts
+++ packages/backend/src/api/collections.ts
@@ -24,13 +24,27 @@ router
   .route('/collections/:uuid')
   .get(authFirst, collectionsMiddleware, async (req, res, next) => {
     try {
+      const CollectionGroupDao = sl.get('CollectionGroupDao');
       const uuid = req.params.uuid as string;
       const user = req.user || null;
-      const page = req.query.page ? ((req.query.page as unknown) as number) : 1;
-      const rows = req.query.rows
-        ? ((req.query.rows as unknown) as number)
-        : 10;
-      const search = req.query.query ? (req.query.query as string) : '';
+      const utils = sl.get('utils');
+
+      const canViewCollection = await CollectionGroupDao.canViewCollection(
+        uuid,
+        req.user
+      );
+      if (!canViewCollection) {
+        next(
+          new HttpForbidden(
+            'You do not have permission to view this collection. If you think this is a mistake, please contact your administrator.'
+          )
+        );
+        return;
+      }
+
+      const { filter: search, limit: rows, page } = utils.extractPagination(
+        req.query
+      );
 
       const HierarchyDao = sl.get('HierarchyDao');
 
@@ -39,15 +53,6 @@ router
         rows,
         search,
       });
-
-      if (response.isForbidden) {
-        next(
-          new HttpForbidden(
-            'You do not have permission to view this collection. If you think this is a mistake, please contact your administrator.'
-          )
-        );
-        return;
-      }
 
       res.json(response);
     } catch (err) {
