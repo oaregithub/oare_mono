86e8575bd783068ddd6ee325d87f8bc7c7d03b9e
bandrewfisher <brett.fisher988@gmail.com>
2021-02-26 13:03:02 -0700

    Migration for adding obj_in_text column (#309)

---
+++ packages/backend/migrations/20210226101018_discourse_obj_in_text.ts
@@ -0,0 +1,25 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasColumn = await knex.schema.hasColumn(
+    'text_discourse',
+    'obj_in_text'
+  );
+  if (!hasColumn) {
+    await knex.schema.table('text_discourse', table => {
+      table.integer('obj_in_text', 5).nullable();
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  const hasColumn = await knex.schema.hasColumn(
+    'text_discourse',
+    'obj_in_text'
+  );
+  if (hasColumn) {
+    await knex.schema.table('text_discourse', table => {
+      table.dropColumn('obj_in_text');
+    });
+  }
+}
