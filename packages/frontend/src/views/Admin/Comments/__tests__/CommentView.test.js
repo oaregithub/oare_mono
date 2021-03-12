a73b8b22cb8cbd0f6dc0a6207d95089112e367ba
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-03-12 10:56:21 -0700

    Updated unit tests and removed test file that is no longer needed.

--- packages/frontend/src/views/Admin/Comments/__tests__/CommentView.test.js
+++ packages/frontend/src/views/Admin/Comments/__tests__/CommentView.test.js
@@ -39,10 +39,7 @@ const renderOptions = {
   localVue,
   vuetify,
   propsData: {
-    threadDisplays: mockThreadDisplays,
-    serverCount: mockServerCount,
-    searchLoading: false,
-    initialLoading: false,
+    isUserComments: false,
   },
 };
 
@@ -66,6 +63,7 @@ const mockRequest = {
     page: 1,
     limit: 10,
   },
+  isUserComments: false,
 };
 
 const mockResponse = {
@@ -94,6 +92,7 @@ describe('CommentView test', () => {
 
   it('successfully retrieves thread displays on load', async () => {
     const wrapper = createWrapper();
+    await flushPromises();
     expect(wrapper.html()).toContain('comment1');
     expect(wrapper.html()).toContain('comment2');
     expect(wrapper.html()).toContain('comment3');
@@ -126,5 +125,151 @@ describe('CommentView test', () => {
       .get('.test-view-all-comments-dialog')
       .get('.test-dialog-title');
     expect(viewAllCommentsDialogTitle.html()).toContain('All Comments');
+  });
+
+  it('successfully retrieves thread displays on load for admins', async () => {
+    createWrapper();
+    await flushPromises();
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith(mockRequest);
+    expect(mockActions.showErrorSnackbar).not.toHaveBeenCalled();
+  });
+
+  it('display error when fails to load thread displays', async () => {
+    sl.set('serverProxy', {
+      getAllThreads: jest
+        .fn()
+        .mockRejectedValue('Unable to retrieve thread displays'),
+    });
+
+    createWrapper();
+    await flushPromises();
+    expect(mockActions.showErrorSnackbar).toHaveBeenCalled();
+  });
+
+  it('filters by status and clears', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const statusFilter = wrapper.get('.test-status-filter input');
+    await statusFilter.trigger('click');
+    const option = wrapper.findAll('.v-list-item__title').at(1);
+    await option.trigger('click');
+    await flushPromises();
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      filters: {
+        ...mockRequest.filters,
+        status: ['In Progress'],
+      },
+    });
+    await wrapper.findAll('button.mdi-close').trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith(mockRequest);
+  });
+
+  it('filters by thread name and clears', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const threadNameFilter = wrapper.get('.test-name-filter input');
+    await threadNameFilter.setValue('testName');
+    await flushPromises();
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      filters: {
+        ...mockRequest.filters,
+        thread: 'testName',
+      },
+    });
+    await wrapper.findAll('button.mdi-close').trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith(mockRequest);
+  });
+
+  it('filters by item and clears', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const itemFilter = wrapper.get('.test-item-filter input');
+    await itemFilter.setValue('testItem');
+    await flushPromises();
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      filters: {
+        ...mockRequest.filters,
+        item: 'testItem',
+      },
+    });
+    await wrapper.findAll('button.mdi-close').trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith(mockRequest);
+  });
+
+  it('filters by comment and clears', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const commentFilter = wrapper.get('.test-comment-filter input');
+    await commentFilter.setValue('testComment');
+    await flushPromises();
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      filters: {
+        ...mockRequest.filters,
+        comment: 'testComment',
+      },
+    });
+    await wrapper.findAll('button.mdi-close').trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith(mockRequest);
+  });
+
+  it('sorts by status', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const statusSort = wrapper.findAll('.v-data-table-header__icon').at(0);
+    await statusSort.trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      sort: {
+        type: 'status',
+        desc: false,
+      },
+    });
+  });
+
+  it('sorts by thread name', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const statusSort = wrapper.findAll('.v-data-table-header__icon').at(1);
+    await statusSort.trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      sort: {
+        type: 'name',
+        desc: false,
+      },
+    });
+  });
+
+  it('sorts by item', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+    const statusSort = wrapper.findAll('.v-data-table-header__icon').at(2);
+    await statusSort.trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      sort: {
+        type: 'item',
+        desc: false,
+      },
+    });
+  });
+
+  it('sort by timestamp', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+
+    const statusSort = wrapper.findAll('.v-data-table-header__icon').at(3);
+    await statusSort.trigger('click');
+    expect(mockServer.getAllThreads).toHaveBeenLastCalledWith({
+      ...mockRequest,
+      sort: {
+        type: 'timestamp',
+        desc: false,
+      },
+    });
   });
 });
