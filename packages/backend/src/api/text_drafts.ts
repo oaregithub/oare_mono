1d3d8e4a4fa673baea529cf1a3b00b2e166542b6
bandrewfisher <brett.fisher988@gmail.com>
2021-03-15 19:58:04 -0600

    367 draft permission (#375)

--- packages/backend/src/api/text_drafts.ts
+++ packages/backend/src/api/text_drafts.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { AddTextDraftPayload } from '@oare/types';
-import { HttpInternalError } from '@/exceptions';
+import { AddTextDraftPayload, TextDraft } from '@oare/types';
+import { HttpBadRequest, HttpInternalError } from '@/exceptions';
 import authenticatedRoute from '@/middlewares/authenticatedRoute';
 import sl from '@/serviceLocator';
 
@@ -10,12 +10,21 @@ router
   .route('/text_drafts/:textUuid')
   .post(authenticatedRoute, async (req, res, next) => {
     const TextDraftsDao = sl.get('TextDraftsDao');
+    const CollectionTextUtils = sl.get('CollectionTextUtils');
 
     try {
       const { textUuid } = req.params;
       const userUuid = req.user!.uuid;
 
       const { content, notes }: AddTextDraftPayload = req.body;
+
+      const canEdit = await CollectionTextUtils.canEditText(textUuid, userUuid);
+      if (!canEdit) {
+        next(
+          new HttpBadRequest('You do not have permission to edit this draft')
+        );
+        return;
+      }
 
       const draft = await TextDraftsDao.getDraft(userUuid, textUuid);
 
@@ -37,7 +46,11 @@ router.route('/text_drafts').get(authenticatedRoute, async (req, res, next) => {
   try {
     const userUuid = req.user!.uuid;
 
-    const drafts = await TextDraftsDao.getAllDrafts(userUuid);
+    const draftUuids = await TextDraftsDao.getAllDraftUuids(userUuid);
+    const drafts: TextDraft[] = await Promise.all(
+      draftUuids.map(uuid => TextDraftsDao.getDraftByUuid(uuid))
+    );
+
     res.json(drafts);
   } catch (err) {
     next(new HttpInternalError(err));
