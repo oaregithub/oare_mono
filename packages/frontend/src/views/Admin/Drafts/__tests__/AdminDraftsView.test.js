dc542eca62291b6d4cf03f2fb2a8a6a7f1d35e04
Brett Fisher <brett.fisher988@gmail.com>
2021-03-26 13:27:56 -0600

    Test sorting

--- packages/frontend/src/views/Admin/Drafts/__tests__/AdminDraftsView.test.js
+++ packages/frontend/src/views/Admin/Drafts/__tests__/AdminDraftsView.test.js
@@ -75,4 +75,38 @@ describe('AdminDraftsView test', () => {
     await wrapper.get('.test-view-content').trigger('click');
     expect(wrapper.find('.test-content-dialog').exists()).toBe(true);
   });
+
+  const expectSort = async (wrapper, index, column, asc = true) => {
+    await wrapper.findAll('th').at(index).trigger('click');
+    await flushPromises();
+
+    expect(server.getAllDrafts).toHaveBeenCalledWith({
+      sortBy: column,
+      sortOrder: asc ? 'asc' : 'desc',
+    });
+  };
+
+  it('sorts by updated', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+
+    await expectSort(wrapper, 2, 'updatedAt', false);
+    await expectSort(wrapper, 2, 'updatedAt');
+  });
+
+  it('sorts by author', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+
+    await expectSort(wrapper, 1, 'author');
+    await expectSort(wrapper, 1, 'author', false);
+  });
+
+  it('sorts by text', async () => {
+    const wrapper = createWrapper();
+    await flushPromises();
+
+    await expectSort(wrapper, 0, 'text');
+    await expectSort(wrapper, 0, 'text', false);
+  });
 });
