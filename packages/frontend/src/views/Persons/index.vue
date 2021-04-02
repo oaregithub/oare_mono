3c3251174925c955217c34c6a84206e80b00b70f
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-04-02 10:06:23 -0600

    Add persons page.

---
+++ packages/frontend/src/views/Persons/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <OareContentView title="People (Prosopographical Index)" :loading="loading">
+    <h1>Persons stuff</h1>
+  </OareContentView>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'PersonsView',
+
+  setup() {
+    const loading = ref(false);
+
+    return {
+      loading,
+    };
+  },
+});
+</script>
