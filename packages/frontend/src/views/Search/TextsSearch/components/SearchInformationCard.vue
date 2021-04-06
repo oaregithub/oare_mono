b24f84fef4e3c1a673a927bd23d3c55e33d02f37
hbludworth <harrisonbludworth@gmail.com>
2021-04-06 12:47:09 -0600

    fixed html ampersand

--- packages/frontend/src/views/Search/TextsSearch/components/SearchInformationCard.vue
+++ packages/frontend/src/views/Search/TextsSearch/components/SearchInformationCard.vue
@@ -12,7 +12,7 @@
           <b>*</b> serves as a wildcard representing any consonant. e.g.: 'a-*a' will search for 'a-ba', 'a-na', 'a-da', etc.
         </li>
         <li>
-          <b>&</b> serves as a wildcard representing various forms of a vowel when placed at the beginning of a sign. e.g.: '&tam' will search for 'tam', 'tám', 'tàm', 'tam₄', etc.
+          <b>&amp;</b> serves as a wildcard representing various forms of a vowel when placed at the beginning of a sign. e.g.: '&amp;tam' will search for 'tam', 'tám', 'tàm', 'tam₄', etc.
         </li>
         <li>
           <b>[]</b> serves as an indicator for a set of specified possible consonants. e.g.: 'a-[bd]a' will search for 'a-ba' and 'a-da'.
@@ -21,7 +21,7 @@
           Searches can be formatted similar to other databases. e.g.: 's,u2-ba1 tam4' will search for 'ṣú-ba tam₄'
         </li>
       </ol>
-      Thus, '&[bdt]a*' would form a valid search that would return results for 'bar', 'dan', 'dam', 'tám', etc. 
+      Thus, '&amp;[bdt]a*' would form a valid search that would return results for 'bar', 'dan', 'dam', 'tám', etc. 
     </v-card>
   </v-menu>
 </template>
