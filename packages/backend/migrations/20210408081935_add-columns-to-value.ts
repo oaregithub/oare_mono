039a8c0fe2e98b922cde9bf6d726bfc6449fae07
Ed Stratford <edstratford@gmail.com>
2021-04-08 08:37:08 -0600

    Add columns to value table

---
+++ packages/backend/migrations/20210408081935_add-columns-to-value.ts
@@ -0,0 +1,39 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasNameColumn = await knex.schema.hasColumn('value', 'name');
+  const hasAbbreviatioColumn = await knex.schema.hasColumn(
+    'value',
+    'abbreviation'
+  );
+
+  if (!hasNameColumn) {
+    await knex.raw(
+      'ALTER TABLE value ADD COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER uuid; '
+    );
+  }
+  if (!hasAbbreviatioColumn) {
+    await knex.raw(
+      'ALTER TABLE value ADD COLUMN abbreviation CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER name;'
+    );
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  const hasNameColumn = await knex.schema.hasColumn('value', 'name');
+  const hasAbbreviationColumn = await knex.schema.hasColumn(
+    'value',
+    'abbreviation'
+  );
+
+  if (hasNameColumn) {
+    await knex.schema.table('value', table => {
+      table.dropColumn('name');
+    });
+  }
+  if (hasAbbreviationColumn) {
+    await knex.schema.table('value', table => {
+      table.dropColumn('abbreviation');
+    });
+  }
+}
