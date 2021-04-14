e64e897519f7f76f069f74618140dddd999b10bf
Tyler Mortensen <tyler@Tylers-MacBook-Pro.local>
2021-04-13 19:36:29 -0600

    linting

--- packages/types/src/people.ts
+++ packages/types/src/people.ts
@@ -2,8 +2,8 @@ import { DisplayableWord } from './dictionary';
 import { EpigraphicTextWithReadings } from './epigraphies';
 
 export interface PersonDisplay extends DisplayableWord {
-  //uuid: 'something', (dictionaryWord's uuid (in order to be able to comment on it))
-  //word: 'Ali-abum' (same as person)
+  // uuid: 'something', (dictionaryWord's uuid (in order to be able to comment on it))
+  // word: 'Ali-abum' (same as person)
   person: string;
   relation: string;
   relationPerson: string;
