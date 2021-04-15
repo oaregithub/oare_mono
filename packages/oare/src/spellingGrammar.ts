92d31bbf202baf038f38fd0f726411e827f24903
Brett Fisher <brett.fisher988@gmail.com>
2021-04-15 15:41:05 -0600

    Update parser to allow for numbers in phrases

--- packages/oare/src/spellingGrammar.ts
+++ packages/oare/src/spellingGrammar.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line quotes
-const numberRegex = `"1/2"|"1/3"|"1/4"|"1/5"|"1/6"|"2/3"|"3/4"|"5/6"|([½⅓¼⅕⅙⅔¾⅚])|([0-9]+("."[0-9]+)?\b)|"LÁ"`;
-
 export default `/* lexical grammar */
 %lex
 %%
@@ -11,9 +8,7 @@ export default `/* lexical grammar */
 "-"                   return '-'
 "(.)"                 return 'DETSEPARATOR'
 "{-}"                 return 'COMPSEPARATOR'
-([0-9]+("."[0-9]+)?\b|"LÁ")("+"([0-9]+("."[0-9]+)?\b|"LÁ"))+ return 'NUMBERPHRASE'
-[0-9]+               return 'DIGITS'
-[\u00C0-\u017FĂAĀÂBDEĒÊGḪHIĪÎYKLMNPQRSṢŠTṬUŪÛÚWZăaāâbdeēêgḫhiīîyklmnpqrsṣštṭuūûúwz,]+([₀₁₂₃₄₅₆₇₈₉]|[0-9]){0,2}  return 'SIGN'
+"1/2"|"1/3"|"1/4"|"1/5"|"1/6"|"2/3"|"3/4"|"5/6"|([½⅓¼⅕⅙⅔¾⅚])|([0-9]+("."[0-9]+)?\b)|[\u00C0-\u017FĂAĀÂBDEĒÊGḪHIĪÎYKLMNPQRSṢŠTṬUŪÛÚWZăaāâbdeēêgḫhiīîyklmnpqrsṣštṭuūûúwz,]+([₀₁₂₃₄₅₆₇₈₉]|[0-9]){0,2}    return 'SIGN'
 "("                 return "("
 ")"                 return ")"
 "{"                 return "{"
@@ -38,13 +33,11 @@ expressions
 separator
   : '.'
   | '-'
+  | '+'
 ;
 
 phrase
-  : opt_detphrase opt_beginning_number signphrase opt_signsuffix
-  | DIGITS compphrase opt_compphrase_suffix
-  | NUMBERPHRASE
-  | DIGITS
+  : opt_detphrase signphrase opt_signsuffix
 ;
 
 opt_compphrase_suffix
@@ -57,15 +50,6 @@ opt_signsuffix
   | /* empty */
 ;
 
-opt_beginning_number
-  : beginning_number
-  | /* empty */
-;
-
-beginning_number
-  : DIGITS separator
-;
-
 signsuffix
   : detphrase
   | compphrase opt_compphrase_suffix
@@ -75,7 +59,6 @@ signphrase
   : signphrase separator SIGN
   | SIGN
 ;
-
 
 opt_detphrase
   : detphrase
