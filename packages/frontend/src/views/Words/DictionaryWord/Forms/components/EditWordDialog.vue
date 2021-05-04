736365e4100199964497c93a4ac9a1e2402f34ee
hbludworth <harrisonbludworth@gmail.com>
2021-05-04 09:56:06 -0600

    added

--- packages/frontend/src/views/Words/DictionaryWord/Forms/components/EditWordDialog.vue
+++ packages/frontend/src/views/Words/DictionaryWord/Forms/components/EditWordDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <oare-dialog
-    :value="value"
-    @input="$emit('input', $event)"
+    v-bind="$attrs"
+    v-on="$listeners"
     :width="2000"
     :persistent="false"
     :closeButton="true"
@@ -103,14 +103,14 @@
         <v-col cols="12" md="6">
           <insert-discourse-rows
             v-if="inDiscourseMode"
-            v-model="value"
+            v-model="$attrs.value"
             :form="form"
             :spelling="spelling"
             :spellingInput="spellingInput"
           />
           <spelling-dialog
             v-if="!inDiscourseMode"
-            v-model="value"
+            v-model="$attrs.value"
             :form="form"
             :spelling="spelling"
             :spellingInput="spellingInput"
@@ -151,10 +151,6 @@ export default defineComponent({
     InsertDiscourseRows,
   },
   props: {
-    value: {
-      type: Boolean,
-      default: false,
-    },
     form: {
       type: Object as PropType<DictionaryForm>,
       required: true,
@@ -168,7 +164,7 @@ export default defineComponent({
       default: true,
     },
   },
-  setup(props) {
+  setup(props, context) {
     const server = sl.get('serverProxy');
     const actions = sl.get('globalActions');
     const _ = sl.get('lodash');
@@ -225,7 +221,7 @@ export default defineComponent({
     });
 
     watch(
-      () => props.value,
+      () => context.attrs.value,
       open => {
         if (!open) {
           if (props.spelling) {
