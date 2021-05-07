3b98cdf2d6bf71da97198fe53532e370f25632df
Brett Fisher <brett.fisher988@gmail.com>
2021-05-06 20:44:37 -0600

    Remove check for auth complete since it's not necessary with firebase

--- packages/frontend/src/navigationGuards/adminGuard.ts
+++ packages/frontend/src/navigationGuards/adminGuard.ts
@@ -1,19 +1,13 @@
 import { NavigationGuard } from 'vue-router';
-import guardRoute from '@/navigationGuards/guardRoute';
 import sl from '@/serviceLocator';
 
 const adminGuard: NavigationGuard = (_to, _from, next) => {
   const store = sl.get('store');
-
-  const navigate = () => {
-    if (!store.getters.isAdmin) {
-      next('/');
-    } else {
-      next();
-    }
-  };
-
-  guardRoute(navigate);
+  if (!store.getters.isAdmin) {
+    next('/');
+  } else {
+    next();
+  }
 };
 
 export default adminGuard;
--- packages/frontend/src/navigationGuards/authenticatedGuard.ts
+++ packages/frontend/src/navigationGuards/authenticatedGuard.ts
@@ -1,19 +1,14 @@
 import { NavigationGuard } from 'vue-router';
-import guardRoute from '@/navigationGuards/guardRoute';
 import sl from '@/serviceLocator';
 
 const authenticatedGuard: NavigationGuard = (_to, _from, next) => {
   const store = sl.get('store');
 
-  const navigate = () => {
-    if (!store.getters.isAuthenticated) {
-      next('/login');
-    } else {
-      next();
-    }
-  };
-
-  guardRoute(navigate);
+  if (!store.getters.isAuthenticated) {
+    next('/login');
+  } else {
+    next();
+  }
 };
 
 export default authenticatedGuard;
