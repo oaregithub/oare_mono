cedcd2a1dc9c2c6faa8278378fcffe6bd0e30320
hbludworth <63749412+hbludworth@users.noreply.github.com>
2021-05-19 17:26:48 -0600

    Changed names/places to use dictionary display (#631)

--- packages/frontend/src/views/Names/index.vue
+++ packages/frontend/src/views/Names/index.vue
@@ -1,31 +1,94 @@
 <template>
   <OareContentView title="Onomasticon" :loading="loading">
-    <NamesPlacesDisplay :wordList="names" :letter="letter" route="names" />
+    <DictionaryDisplay
+      :wordList="names"
+      :letter="letter"
+      route="names"
+      :searchFilter="searchFilter"
+    >
+      <template #word="{ word }">
+        <router-link :to="`/namesWord/${word.uuid}`" class="mr-1">{{
+          word.word
+        }}</router-link>
+      </template>
+      <template #translation="{ word }">
+        <div>
+          {{
+            word.translations.length > 0
+              ? word.translations[0].translation
+              : '(no trans.)'
+          }}
+        </div>
+      </template>
+      <template #forms="{ word }">
+        <div
+          v-for="(formInfo, idx) in word.forms"
+          :key="idx"
+          class="d-flex flex-wrap pl-4"
+        >
+          <em class="font-weight-bold mr-1">
+            {{ formInfo.form }}
+          </em>
+
+          <div class="mr-1">({{ formInfo.cases.join('/') }})</div>
+          <router-link
+            v-if="formInfo.spellings.length > 0"
+            :to="`/namesWord/${word.uuid}`"
+            class="mr-1 text-decoration-none"
+            >({{ formInfo.spellings.length }})</router-link
+          >
+        </div>
+      </template>
+    </DictionaryDisplay>
   </OareContentView>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, Ref, watch } from '@vue/composition-api';
+import DictionaryDisplay from '@/components/DictionaryDisplay/index.vue';
 import { Word } from '@oare/types';
-import NamesPlacesDisplay from '@/components/NamesPlacesDisplay/index.vue';
 import sl from '@/serviceLocator';
 
 export default defineComponent({
   name: 'NamesView',
+  components: {
+    DictionaryDisplay,
+  },
   props: {
     letter: {
       type: String,
       required: true,
     },
   },
-  components: {
-    NamesPlacesDisplay,
-  },
+
   setup(props) {
-    const names: Ref<Word[]> = ref([]);
-    const loading = ref(false);
     const server = sl.get('serverProxy');
     const actions = sl.get('globalActions');
+
+    const names: Ref<Word[]> = ref([]);
+    const loading = ref(false);
+
+    const searchFilter = (search: string, word: Word): boolean => {
+      const lowerSearch = search ? search.toLowerCase() : '';
+
+      return (
+        word.word.toLowerCase().includes(lowerSearch) ||
+        word.translations.some(tr =>
+          tr.translation.toLowerCase().includes(lowerSearch)
+        ) ||
+        word.forms.some(form => {
+          return (
+            form.form.toLowerCase().includes(lowerSearch) ||
+            form.spellings.some(spelling => {
+              return (
+                spelling &&
+                spelling.spelling.toLowerCase().includes(lowerSearch)
+              );
+            })
+          );
+        })
+      );
+    };
 
     watch(
       () => props.letter,
@@ -45,9 +108,8 @@ export default defineComponent({
     return {
       names,
       loading,
+      searchFilter,
     };
   },
 });
 </script>
-
-<style></style>
