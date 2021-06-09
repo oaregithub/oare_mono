3d89ac17474c4e367335efca4599e6e0fb3a6acc
Brett Fisher <brett.fisher988@gmail.com>
2021-06-09 12:01:52 -0600

    Remove old query param

--- packages/frontend/src/hooks/useQueryParam.ts
+++ packages/frontend/src/hooks/useQueryParam.ts
@@ -10,42 +10,6 @@ const getUrlParams = () => {
 const getQueryStringVal = (key: string): string | null =>
   getUrlParams().get(key);
 
-// const useQueryParam = (
-//   key: string,
-//   defaultVal: string
-// ): [Ref<string>, (val: string) => void] => {
-//   const param = ref(getQueryStringVal(key) || defaultVal);
-
-//   window.addEventListener(`update:${key}`, e => {
-//     const { newVal } = (e as CustomEvent).detail;
-//     param.value = newVal;
-
-//     const urlParams = getUrlParams();
-
-//     if (newVal && newVal.trim() !== '') {
-//       urlParams.set(key, newVal);
-//     } else {
-//       urlParams.delete(key);
-//     }
-
-//     if (typeof window !== 'undefined') {
-//       const { protocol, pathname, host } = window.location;
-//       const newUrl = `${protocol}//${host}${pathname}?${urlParams.toString()}`;
-//       window.history.pushState({}, '', newUrl);
-//     }
-//   });
-
-//   const updateParam = (newVal: string) => {
-//     window.dispatchEvent(
-//       new CustomEvent(`update:${key}`, {
-//         detail: { newVal },
-//       })
-//     );
-//   };
-
-//   return [param, updateParam];
-// };
-
 const useQueryParam = (key: string, defaultVal: string): Ref<string> => {
   const param = ref(getQueryStringVal(key) || defaultVal);
 
