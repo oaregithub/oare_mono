06a8605e5833f8cadd15dd27cba994cc37a497ca
Ed Stratford <edstratford@gmail.com>
2021-06-21 15:56:48 -0600

    hoping my linting is better

--- packages/backend/migrations/20210621141149_add-columns-to-period.ts
+++ packages/backend/migrations/20210621141149_add-columns-to-period.ts
@@ -6,11 +6,26 @@ export async function up(knex: Knex): Promise<void> {
     'period',
     'abbreviation'
   );
-  const hasOfficial1Column = await knex.schema.hasColumn('period', 'official1_uuid');
-  const hasOfficial2Column = await knex.schema.hasColumn('period', 'official2_uuid');
-  const hasOfficialPN1Column = await knex.schema.hasColumn('period', 'officialPN1_uuid');
-  const hasOfficialPN2Column = await knex.schema.hasColumn('period', 'officialPN2_uuid');
-  const hasPeriodTypeColumn = await knex.schema.hasColumn('period', 'period_type');
+  const hasOfficial1Column = await knex.schema.hasColumn(
+    'period',
+    'official1_uuid'
+  );
+  const hasOfficial2Column = await knex.schema.hasColumn(
+    'period',
+    'official2_uuid'
+  );
+  const hasOfficialPN1Column = await knex.schema.hasColumn(
+    'period',
+    'officialPN1_uuid'
+  );
+  const hasOfficialPN2Column = await knex.schema.hasColumn(
+    'period',
+    'officialPN2_uuid'
+  );
+  const hasPeriodTypeColumn = await knex.schema.hasColumn(
+    'period',
+    'period_type'
+  );
 
   if (!hasNameColumn) {
     await knex.raw(
@@ -22,31 +37,31 @@ export async function up(knex: Knex): Promise<void> {
       'ALTER TABLE period ADD COLUMN abbreviation CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER name;'
     );
   }
-    if (!hasOfficial1Column) {
+  if (!hasOfficial1Column) {
     await knex.raw(
       'ALTER TABLE period ADD COLUMN official1_uuid CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER abbreviation; '
     );
-    }
-    if (!hasOfficial2Column) {
+  }
+  if (!hasOfficial2Column) {
     await knex.raw(
       'ALTER TABLE period ADD COLUMN official2_uuid CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER official1_uuid; '
     );
   }
-    if (!hasOfficialPN1Column) {
+  if (!hasOfficialPN1Column) {
     await knex.raw(
       'ALTER TABLE period ADD COLUMN official1_uuid CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER official2_uuid; '
     );
   }
-    if (!hasOfficialPN2Column) {
+  if (!hasOfficialPN2Column) {
     await knex.raw(
       'ALTER TABLE period ADD COLUMN official2_uuid CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER officialPN1_uuid; '
     );
-    }
-   if (!hasPeriodTypeColumn) {
+  }
+  if (!hasPeriodTypeColumn) {
     await knex.raw(
       'ALTER TABLE period ADD COLUMN period_type CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER officialPN2_uuid; '
     );
-  }   
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
@@ -55,12 +70,27 @@ export async function down(knex: Knex): Promise<void> {
     'period',
     'abbreviation'
   );
-  const hasOfficial1Column = await knex.schema.hasColumn('period', 'official1_uuid');
-  const hasOfficial2Column = await knex.schema.hasColumn('period', 'official2_uuid');
-  const hasOfficialPN1Column = await knex.schema.hasColumn('period', 'officialPN1_uuid');
-  const hasOfficialPN2Column = await knex.schema.hasColumn('period', 'officialPN2_uuid');
-  const hasPeriodTypeColumn = await knex.schema.hasColumn('period', 'period_type');
-    
+  const hasOfficial1Column = await knex.schema.hasColumn(
+    'period',
+    'official1_uuid'
+  );
+  const hasOfficial2Column = await knex.schema.hasColumn(
+    'period',
+    'official2_uuid'
+  );
+  const hasOfficialPN1Column = await knex.schema.hasColumn(
+    'period',
+    'officialPN1_uuid'
+  );
+  const hasOfficialPN2Column = await knex.schema.hasColumn(
+    'period',
+    'officialPN2_uuid'
+  );
+  const hasPeriodTypeColumn = await knex.schema.hasColumn(
+    'period',
+    'period_type'
+  );
+
   if (hasNameColumn) {
     await knex.schema.table('period', table => {
       table.dropColumn('name');
@@ -76,24 +106,24 @@ export async function down(knex: Knex): Promise<void> {
       table.dropColumn('official1_uuid');
     });
   }
-    if (hasOfficial2Column) {
+  if (hasOfficial2Column) {
     await knex.schema.table('period', table => {
       table.dropColumn('official2_uuid');
     });
   }
-    if (hasOfficialPN1Column) {
+  if (hasOfficialPN1Column) {
     await knex.schema.table('period', table => {
       table.dropColumn('officialPN1_uuid');
     });
   }
-    if (hasOfficialPN2Column) {
+  if (hasOfficialPN2Column) {
     await knex.schema.table('period', table => {
       table.dropColumn('officialPN2_uuid');
     });
   }
-    if (hasPeriodTypeColumn) {
+  if (hasPeriodTypeColumn) {
     await knex.schema.table('period', table => {
       table.dropColumn('period_type');
     });
-  }  
+  }
 }
