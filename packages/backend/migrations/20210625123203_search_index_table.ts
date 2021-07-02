3a92de1f5134d0a97143714804c603f130795a3c
Brett Fisher <brett.fisher988@gmail.com>
2021-07-02 12:45:46 -0600

    Add table in search table index migration

--- packages/backend/migrations/20210625123203_search_index_table.ts
+++ packages/backend/migrations/20210625123203_search_index_table.ts
@@ -70,6 +70,26 @@ async function indexText(knex: Knex, { textUuid, textName }: TextRow) {
 }
 
 export async function up(knex: Knex): Promise<void> {
+  const hasTable = await knex.schema.hasTable('search_index');
+  if (hasTable) {
+    return;
+  }
+  await knex.schema.createTable('search_index', table => {
+    table.increments('id');
+    table.string('sign_uuid_sequence', 1332);
+    table.uuid('text_uuid');
+    table.string('text_name');
+    table.decimal('line');
+    table.text('line_reading');
+
+    table.foreign('text_uuid').references('text.uuid');
+  });
+
+  // Index keys will be too long if every column is utf8, so just change line_reading
+  await knex.raw(
+    'ALTER TABLE search_index MODIFY line_reading TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin'
+  );
+
   const texts: TextRow[] = await knex('text').select(
     'uuid AS textUuid',
     'name AS textName'
@@ -89,5 +109,15 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
+  /**
+   * Delete first since I was having problems with storage
+   * not being freed even after dropping the table
+   */
   await knex('search_index').del();
+
+  const hasTable = await knex.schema.hasTable('search_index');
+
+  if (hasTable) {
+    await knex.schema.dropTableIfExists('search_index');
+  }
 }
