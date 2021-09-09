5ba59168aaee1b822cbdcbafb0279b836837958d
edstratford <edstratford@gmail.com>
2021-09-09 08:32:58 -0600

    add container column to resource

---
+++ packages/backend/migrations/20210909082646_add_col_resource.ts
@@ -0,0 +1,27 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasContainerColumn = await knex.schema.hasColumn(
+    'resource',
+    'container'
+  );
+
+  if (!hasContainerColumn) {
+    await knex.raw(
+      'ALTER TABLE resource ADD COLUMN container VARCHAR CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER type; '
+    );
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  const hasContainerColumn = await knex.schema.hasColumn(
+    'resource',
+    'container'
+  );
+
+  if (hasContainerColumn) {
+    await knex.schema.table('resource', table => {
+      table.dropColumn('container');
+    });
+  }
+}
