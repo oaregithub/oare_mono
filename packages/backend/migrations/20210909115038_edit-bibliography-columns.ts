74e8b0148d57295f8e9e8e882b870aa219435dad
edstratford <edstratford@gmail.com>
2021-09-09 22:53:43 -0600

    some linting fixed..maybe

--- packages/backend/migrations/20210909115038_edit-bibliography-columns.ts
+++ packages/backend/migrations/20210909115038_edit-bibliography-columns.ts
@@ -5,42 +5,38 @@ export async function up(knex: Knex): Promise<void> {
     'bibliography',
     'citationFormat'
   );
-    
+
   const hasTypeColumn = await knex.schema.hasColumn(
     'bibliography',
-    'type'
-  );
-    
+    'type');
+
   const hasYearColumn = await knex.schema.hasColumn(
     'bibliography',
-    'year'
-  );
-    
+    'year');
+
   const hasMonthColumn = await knex.schema.hasColumn(
     'bibliography',
-    'month'
-  );
-    
+    'month');
+
   const hasDayColumn = await knex.schema.hasColumn(
     'bibliography',
-    'day'
-  );
-    
+    'day');
+
   const hasEndYearColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_year'
   );
-    
+
   const hasEndMonthColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_month'
   );
-    
+
   const hasEndDayColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_day'
   );
-    
+
   const hasZotItemKeyColumn = await knex.schema.hasColumn(
     'bibliography',
     'zot_item_key'
@@ -51,49 +47,49 @@ export async function up(knex: Knex): Promise<void> {
       table.renameColumn('citationFormat', 'short_cit');
     });
   }
-    
+
   if (hasTypeColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('type');
     });
   }
-    
+
   if (hasYearColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('year');
     });
   }
-    
+
   if (hasMonthColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('month');
     });
   }
-    
+
   if (hasDayColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('day');
     });
   }
-    
+
   if (hasEndYearColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('end_year');
     });
   }
-    
+
   if (hasEndMonthColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('end_month');
     });
   }
-    
+
   if (hasEndDayColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('end_day');
     });
   }
-    
+
   if (!hasZotItemKeyColumn) {
     await knex.raw(
       'ALTER TABLE bibliography ADD COLUMN zot_item_key VARCHAR(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER uuid; '
@@ -109,39 +105,35 @@ export async function down(knex: Knex): Promise<void> {
 
   const hasTypeColumn = await knex.schema.hasColumn(
     'bibliography',
-    'type'
-  );
-    
+    'type');
+
   const hasYearColumn = await knex.schema.hasColumn(
     'bibliography',
-    'year'
-  );
-    
+    'year');
+
   const hasMonthColumn = await knex.schema.hasColumn(
     'bibliography',
-    'month'
-  );
-    
+    'month');
+
   const hasDayColumn = await knex.schema.hasColumn(
     'bibliography',
-    'day'
-  );
-    
+    'day');
+
   const hasEndYearColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_year'
   );
-    
+
   const hasEndMonthColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_month'
   );
-    
+
   const hasEndDayColumn = await knex.schema.hasColumn(
     'bibliography',
     'end_day'
   );
-    
+
   const hasZotItemKeyColumn = await knex.schema.hasColumn(
     'bibliography',
     'zot_item_key'
@@ -158,25 +150,25 @@ export async function down(knex: Knex): Promise<void> {
       'ALTER TABLE bibliography ADD COLUMN type VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER uuid; '
     );
   }
-    
+
   if (!hasYearColumn) {
     await knex.raw(
       'ALTER TABLE bibliography ADD COLUMN year INT(4) AFTER citationFormat; '
     );
   }
-    
+
   if (!hasMonthColumn) {
     await knex.raw(
       'ALTER TABLE bibliography ADD COLUMN month INT(2) AFTER year; '
     );
   }
-    
+
   if (!hasDayColumn) {
     await knex.raw(
       'ALTER TABLE bibliography ADD COLUMN day INT(2) AFTER month; '
     );
   }
-    
+
   if (!hasEndYearColumn) {
     await knex.raw(
       'ALTER TABLE bibliography ADD COLUMN end_year INT(4) AFTER day; '
@@ -194,7 +186,7 @@ export async function down(knex: Knex): Promise<void> {
       'ALTER TABLE bibliography ADD COLUMN end_day INT(2) AFTER end_month; '
     );
   }
-    
+
   if (hasZotItemKeyColumn) {
     await knex.schema.table('bibliography', table => {
       table.dropColumn('zot_item_key');
