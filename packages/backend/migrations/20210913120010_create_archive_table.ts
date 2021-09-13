1eeabf142c9adc0aad805969ae632962347b7a99
edstratford <edstratford@gmail.com>
2021-09-13 16:35:15 -0600

    create archive table (#860)

---
+++ packages/backend/migrations/20210913120010_create_archive_table.ts
@@ -0,0 +1,26 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasArchiveTable = await knex.schema.hasTable('archive');
+
+  if (!hasArchiveTable) {
+    await knex.schema.createTable('archive', table => {
+      table.charset('utf8');
+      table.collate('utf8_general_ci');
+      table.increments('id').primary();
+      table.uuid('uuid').notNullable().unique();
+      table.uuid('owner').nullable();
+      table.uuid('current_editor').nullable();
+      table.uuid('arch_locus').nullable();
+      table.string('type', 13).notNullable();
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  const hasArchiveTable = await knex.schema.hasTable('archive');
+
+  if (hasArchiveTable) {
+    await knex.schema.dropTable('archive');
+  }
+}
