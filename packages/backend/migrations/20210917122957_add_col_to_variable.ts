773b9fb6b19d4942546696662b9ea75407bb5122
edstratford <edstratford@gmail.com>
2021-09-20 17:33:45 -0600

    Add column to variable (#866)
    * Add column to variable
    
    * small lint changes

---
+++ packages/backend/migrations/20210917122957_add_col_to_variable.ts
@@ -0,0 +1,27 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  const hasTableReferenceColumn = await knex.schema.hasColumn(
+    'variable',
+    'table_reference'
+  );
+
+  if (!hasTableReferenceColumn) {
+    await knex.raw(
+      'ALTER TABLE variable ADD COLUMN table_reference VARCHAR(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin AFTER abbreviation; '
+    );
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  const hasTableReferenceColumn = await knex.schema.hasColumn(
+    'variable',
+    'table_reference'
+  );
+
+  if (hasTableReferenceColumn) {
+    await knex.schema.table('variable', table => {
+      table.dropColumn('table_reference');
+    });
+  }
+}
