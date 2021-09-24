e3184aedcb8bc837806566bfd550cce9719a5398
edstratford <edstratford@gmail.com>
2021-09-24 16:19:51 -0600

    LandingPageUpdates

--- packages/frontend/src/views/Home/LandingPage.vue
+++ packages/frontend/src/views/Home/LandingPage.vue
@@ -3,11 +3,11 @@
     <v-row justify="center" class="title font-weight-regular">
       <v-col cols="12" lg="10">
         <p>
-          Welcome to a pre-release of OARE (pronounced /’ôrə/) at
+          Welcome to a pre-release of OARE (/’ôrə/) at
           <a href="https://oare.byu.edu">oare.byu.edu</a>.
         </p>
         <p>
-          This website aims to offer published Old Assyrian texts for viewing,
+          This website aims to offer published Old Assyrian texts for viewing
           and eventually analysis.
         </p>
         <p>
@@ -15,7 +15,7 @@
           Old Assyrian texts from the period of Kültepe Level II and Kültepe
           Level Ib. OARE is organized to record textual and expert observations
           arising from the interpretation, In this manner, it is intended
-          eventually facilitate collaborative research projects on the
+          to facilitate collaborative research projects on the
           historical and archaeological record of the Old Assyrian trade and
           society through linguistic, lexical, economic, social, and other
           lenses.
@@ -30,26 +30,26 @@
         <p>
           At present, only transliterations are available here. An initial
           onomasticon, lexicon, gazetteer, and temporal period index are in
-          preparation for release as well. Asset and transaction index, and
-          other indexes are planned.
+          preparation for release. Asset and transaction indexes, a signlist and
+          bibliography are also planned.
         </p>
         <p>
           The sidebar search on the left prefers abbreviations used within the
           Old Assyrian specialist community (thus TC 2 instead of TCL 14) and
           only uses commas with reference to page numbers (thus TC 2 1 instead
-          of TC 2, 1). Plans to make the searching more forgiving are in the
-          works.
+          of TC 2, 1). Consult the (i) buttons there and in the Search page to 
+          find further search features.
         </p>
         <p class="blue--text text--lighten-1">
           <em>
             Please be advised, we are still working out
-            <router-link to="/issues">kinks</router-link>. Use transliterations
-            at your own risk at this point. Many texts display nicely, but some
-            do not. In addition, we are still auditing the data. Original
-            transliterations of text published before roughly 2008 arise from
-            the Old Assyrian Text Project and sources drawn to create that
-            project. Transliterations from more recent publications were checked
-            within OARE/OCHRE, but some errors were introduced in converting to
+            <router-link to="/issues">issues</router-link>. Use transliterations
+            at your own risk. Many texts display nicely, but some display errors
+            occasionally occur. In addition, we are still auditing the data. 
+            Original transliterations of text published before roughly 2008
+            arise from the Old Assyrian Text Project and a few other sources.
+            Transliterations from more recent publications were checked within 
+            OARE/OCHRE, but some errors were introduced in converting to
             the format used for this database.</em
           >
         </p>
@@ -58,9 +58,12 @@
           displays the current status of the transliteration. The vast majority
           are ‘yellow’ meaning the transliteration has not yet been checked.
           Hovering over the traffic light icon will indicate what the displayed
-          icon means.
+          colors mean for reliability of the transliteration.
         </p>
         <p>
+          If you are interested in participating in the project, please consider
+          creating an account. Passwords chosen are not visible to us. Registering
+          allows up to share Beta features for those interested. 
           Feedback mechanisms for also in development so that users can mark
           texts that are misbehaving, or point out errors. In the meantime,
           please send any feedback to
