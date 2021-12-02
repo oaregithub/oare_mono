bd68968c5072257a73421ce233c210a637a6d1d6
10808249 <jhjun98@gmail.com>
2021-12-02 07:56:10 -0700

    minor fixes by feedback (2)

--- packages/frontend/src/views/About/index.vue
+++ packages/frontend/src/views/About/index.vue
@@ -1,64 +1,73 @@
 <template>
-    <div>
-        <span>
-            oare.byu.edu is an ongoing project with the aim to becoming a valuable research environment for Old Assyrian
-            Studies. The database is inspired by the Online Cultural Heritage Research Environment (ochre.uchicago.edu) but
-            is a simplified, SQL implementation of some of the concepts developed there, with some particular tools
-            developed specifically for editing cuneiform texts and the extension of projects involved in articulating the
-            syntactic relationships of words, phrases, clauses, etc. and by extension, the persons, assets, transactions,
-            and events they recorded.
-        </span>
-    
-        <span>
-            To read more here about the data structure of oare.
-        </span>
-    
-        <span>
-            oare.byu.edu was organized and directed by Edward Stratford, Brigham Young University, but is the beneficiary of
-            help from a great number of other individuals.
-        </span>
-    
-        <span>
-            Roughly half the published texts available on the website were first digitized by Karl Hecker and made available
-            to the Old Assyrian Text project. Without his first efforts, this project would be far behind where it is now.
-            Other members of the Old Assyrian Text Project also contributed to the accumulation of digitized forms of texts
-            and deserve great thanks: Cecile Michele, Gojko Barjamovic, Guido Kryszat, Jan Gerrit Dercksen, Klaas Veenhof,
-            Mogens Trolle Larsen, Thomas Hertel.
-        </span>
-    
-        <span>
-            A number of other specialists have kindly offered comments and feedback, including Bert Kouwenberg and Hakan
-            Erol.
-        </span>
-    
-        <span>
-            Jonathon Hanna (BYU student) converted the OCHRE export into SQL and helped shepherd the post-conversion cleanup
-            and SQL implementation of the dataset. Brett Fisher (BYU student, graduated) setting up the JS project, the AWS
-            hosting arrangement, integration of the SQL and the first major iteration of the site. Harrison Bludworth (BYU
-            student) has overseen the project starting summer 2021, and implemented numerous improvements, including the
-            import and edit modules.
-        </span>
-    
-        <span>
-            Thanks also to Tyler Mortenson, Jooyeon Park, Justis Brown, Kyungwoo Jo, and Hongjoon Jun for the full stack
-            development.
-        </span>
-    
-        <span>
-            A significant number of students from BYU participated in enhancing the textual data while in OCHRE:
-            Alexa Blankenship, Allison Malmrose, Audrey Gabrielson, Brenton Erickson, Britta Johnson, Brittany Wallace,
-            Catie Ashley, Chandler Helvey, Christina Nelson, Colton Kunz, Courtney Dotson, David Ferguson, David Ridge,
-            David Romney, Emily Peterson, Emily Yankura, Jackie Davis, Jacob Murphy, Jake Inman, James Conway, Jason
-            Saunders, Jeremy Madsen, Jeremy Schone, Jessica Dildine, Jessica Smith, Jessica Brown, JoLynne Minnick, Jonathon
-            Nelson, Jordan Oseen, Kaili Sparks, Karlie Alldredge, Katherine Morse, Kayli Thomson, Kelsie Cannon, Lara
-            Mayfield, Leonor Arnesen, Matilde Oreilly, Matthew Lin, McKenzi Christensen, Merna Raines, Michael Godfrey,
-            Michael Trotter, Michaela Shurts, Natalie Merten, Nathan May, Nora Corcoran, Paul Gauthier, Ryan Winters, Sarah
-            Rounsville, Stephanie Moffit, Stine Plomgren, Stuart Cooke, Thomas Richins, Trevor Murphy, Tyler Harris
-        </span>
-    
-        <span>
-            Thank you to the following who prepared and imported texts into the OCHRE project:
-            Andrew Dix, Annie Schloen, Jae-Hwan Kim.
-        </span>
+  <OareContentView>
+    <div class="title font-weight-regular">
+      <p>
+        oare.byu.edu is an ongoing project with the aim to becoming a valuable research
+        environment for Old Assyrian Studies. The database is inspired by the Online
+        Cultural Heritage Research Environment (ochre.uchicago.edu) but is a simplified,
+        SQL implementation of some of the concepts developed there, with some particular
+        tools developed specifically for editing cuneiform texts and the extension of
+        projects involved in articulating the syntactic relationships of words, phrases,
+        clauses, etc. and by extension, the persons, assets, transactions, and events they
+        recorded.
+      </p>
+
+      <p>To read more here about the data structure of oare.</p>
+
+      <p>
+        oare.byu.edu was organized and directed by Edward Stratford, Brigham Young
+        University, but is the beneficiary of help from a great number of other
+        individuals.
+      </p>
+
+      <p>
+        Roughly half the published texts available on the website were first digitized by
+        Karl Hecker and made available to the Old Assyrian Text project. Without his first
+        efforts, this project would be far behind where it is now. Other members of the
+        Old Assyrian Text Project also contributed to the accumulation of digitized forms
+        of texts and deserve great thanks: Cecile Michele, Gojko Barjamovic, Guido
+        Kryszat, Jan Gerrit Dercksen, Klaas Veenhof, Mogens Trolle Larsen, Thomas Hertel.
+      </p>
+
+      <p>
+        A number of other specialists have kindly offered comments and feedback, including
+        Bert Kouwenberg and Hakan Erol.
+      </p>
+
+      <p>
+        Jonathon Hanna (BYU student) converted the OCHRE export into SQL and helped
+        shepherd the post-conversion cleanup and SQL implementation of the dataset. Brett
+        Fisher (BYU student, graduated) setting up the JS project, the AWS hosting
+        arrangement, integration of the SQL and the first major iteration of the site.
+        Harrison Bludworth (BYU student) has overseen the project starting summer 2021,
+        and implemented numerous improvements, including the import and edit modules.
+      </p>
+
+      <p>
+        Thanks also to Tyler Mortenson, Jooyeon Park, Justis Brown, Kyungwoo Jo, and
+        Hongjoon Jun for the full stack development.
+      </p>
+
+      <p>
+        A significant number of students from BYU participated in enhancing the textual
+        data while in OCHRE: Alexa Blankenship, Allison Malmrose, Audrey Gabrielson,
+        Brenton Erickson, Britta Johnson, Brittany Wallace, Catie Ashley, Chandler Helvey,
+        Christina Nelson, Colton Kunz, Courtney Dotson, David Ferguson, David Ridge, David
+        Romney, Emily Peterson, Emily Yankura, Jackie Davis, Jacob Murphy, Jake Inman,
+        James Conway, Jason Saunders, Jeremy Madsen, Jeremy Schone, Jessica Dildine,
+        Jessica Smith, Jessica Brown, JoLynne Minnick, Jonathon Nelson, Jordan Oseen,
+        Kaili Sparks, Karlie Alldredge, Katherine Morse, Kayli Thomson, Kelsie Cannon,
+        Lara Mayfield, Leonor Arnesen, Matilde Oreilly, Matthew Lin, McKenzi Christensen,
+        Merna Raines, Michael Godfrey, Michael Trotter, Michaela Shurts, Natalie Merten,
+        Nathan May, Nora Corcoran, Paul Gauthier, Ryan Winters, Sarah Rounsville,
+        Stephanie Moffit, Stine Plomgren, Stuart Cooke, Thomas Richins, Trevor Murphy,
+        Tyler Harris
+      </p>
+
+      <p>
+        Thank you to the following who prepared and imported texts into the OCHRE project:
+        Andrew Dix, Annie Schloen, Jae-Hwan Kim.
+      </p>
     </div>
+  </OareContentView>
 </template>
