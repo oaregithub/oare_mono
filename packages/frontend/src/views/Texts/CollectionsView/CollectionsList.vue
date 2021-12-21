18ef481279596da660f8c67b1e17ebf0732d1fca
kyungwooJo <35247917+kyungwooJo@users.noreply.github.com>
2021-12-22 04:13:56 +0900

    Added three more collection information (Excavation Info, Museum Info… (#1058)
    * Added three more collection information (Excavation Info, Museum Info, Primary Publication Info)
    
    * merge problem handled
    
    Co-authored-by: Kyungwoo scott Jo <scottkjo@Kyungwooui-MacBookPro.local>
    Co-authored-by: Kyungwoo scott Jo <scottkjo@KyungwocBookPro.app.byu.edu>

--- packages/frontend/src/views/Texts/CollectionsView/CollectionsList.vue
+++ packages/frontend/src/views/Texts/CollectionsView/CollectionsList.vue
@@ -21,10 +21,7 @@
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  PropType,
-} from '@vue/composition-api';
+import { defineComponent, PropType } from '@vue/composition-api';
 import { Collection } from '@oare/types';
 import { letterGroups } from './utils';
 
