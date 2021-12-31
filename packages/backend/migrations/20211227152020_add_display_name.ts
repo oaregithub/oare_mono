349cc7f87b1a95f444a9509372fad74b59aeb393
justisbrown <64491880+justisbrown@users.noreply.github.com>
2021-12-31 14:01:19 -0700

    small changes

--- packages/backend/migrations/20211227152020_add_display_name.ts
+++ packages/backend/migrations/20211227152020_add_display_name.ts
@@ -1,78 +1,84 @@
-import * as Knex from "knex";
+import * as Knex from 'knex';
 
 export interface Text {
-    name: string,
-    excavation_prfx: string,
-    excavation_no: string,
-    publication_prfx: string,
-    publication_no: string,
-    museum_prfx: string,
-    museum_no: string,
-    uuid: string,
-    id: Number
+  name: string;
+  excavation_prfx: string;
+  excavation_no: string;
+  publication_prfx: string;
+  publication_no: string;
+  museum_prfx: string;
+  museum_no: string;
+  uuid: string;
+  id: Number;
 }
 
 export async function up(knex: Knex): Promise<void> {
-    const hasDisplayName = await knex.schema.hasColumn('text', 'display_name');
-    const Texts: Text[] = await knex.from('text')
-        .select('name', 'excavation_prfx', 'excavation_no', 'publication_prfx', 'publication_no', 'museum_prfx', 'museum_no', 'uuid', 'id')
-    
-    console.log(Texts[0])
-    
-    
-    if (!hasDisplayName) {
-        await knex.schema.table('text', table => {
-        table.string('display_name');
-        });
+  const hasDisplayName = await knex.schema.hasColumn('text', 'display_name');
+  const Texts: Text[] = await knex
+    .from('text')
+    .select(
+      'name',
+      'excavation_prfx',
+      'excavation_no',
+      'publication_prfx',
+      'publication_no',
+      'museum_prfx',
+      'museum_no',
+      'uuid',
+      'id'
+    );
+
+  if (!hasDisplayName) {
+    await knex.schema.table('text', table => {
+      table.string('display_name');
+    });
+  }
+  async function generateDisplayName(text: Text) {
+    let displayName = '';
+    if (
+      text.excavation_prfx &&
+      text.excavation_prfx.slice(0, 2).toLowerCase() === 'kt'
+    ) {
+      displayName = `${text.excavation_prfx} ${text.excavation_no}`;
+      if (text.publication_prfx && text.publication_no) {
+        displayName += ` (${text.publication_prfx} ${text.publication_no})`;
+      } else if (text.museum_prfx && text.museum_no) {
+        displayName += ` (${text.museum_prfx} ${text.museum_no})`;
+      }
+    } else if (text.publication_prfx && text.publication_no) {
+      displayName = `${text.publication_prfx} ${text.publication_no}`;
+      if (text.excavation_prfx && text.excavation_no) {
+        displayName += ` (${text.excavation_prfx} ${text.excavation_no})`;
+      } else if (text.museum_prfx && text.museum_no) {
+        displayName += ` (${text.museum_prfx} ${text.museum_no})`;
+      }
+    } else if (text.excavation_prfx && text.excavation_no) {
+      displayName = `${text.excavation_prfx} ${text.excavation_no}`;
+      if (text.museum_prfx && text.museum_no) {
+        displayName += ` (${text.museum_prfx} ${text.museum_no})`;
+      }
+    } else if (text.museum_prfx && text.museum_no) {
+      displayName = `${text.museum_prfx} ${text.museum_no}`;
+    } else {
+      displayName = text.name;
     }
-    for (let i = 0; i < Texts.length; i++){
-            let displayName = ''
-            if (
-            Texts[i].excavation_prfx &&
-            Texts[i].excavation_prfx.slice(0, 2).toLowerCase() === 'kt'
-            ) {
-            displayName = `${Texts[i].excavation_prfx} ${Texts[i].excavation_no}`;
-            if (Texts[i].publication_prfx && Texts[i].publication_no) {
-                displayName += ` (${Texts[i].publication_prfx} ${Texts[i].publication_no})`;
-            } else if (Texts[i].museum_prfx && Texts[i].museum_no) {
-                displayName += ` (${Texts[i].museum_prfx} ${Texts[i].museum_no})`;
-            }
-            } else if (Texts[i].publication_prfx && Texts[i].publication_no) {
-            displayName = `${Texts[i].publication_prfx} ${Texts[i].publication_no}`;
-            if (Texts[i].excavation_prfx && Texts[i].excavation_no) {
-                displayName += ` (${Texts[i].excavation_prfx} ${Texts[i].excavation_no})`;
-            } else if (Texts[i].museum_prfx && Texts[i].museum_no) {
-                displayName += ` (${Texts[i].museum_prfx} ${Texts[i].museum_no})`;
-            }
-            } else if (Texts[i].excavation_prfx && Texts[i].excavation_no) {
-            displayName = `${Texts[i].excavation_prfx} ${Texts[i].excavation_no}`;
-            if (Texts[i].museum_prfx && Texts[i].museum_no) {
-                displayName += ` (${Texts[i].museum_prfx} ${Texts[i].museum_no})`;
-            }
-            } else if (Texts[i].museum_prfx && Texts[i].museum_no) {
-            displayName = `${Texts[i].museum_prfx} ${Texts[i].museum_no}`;
-            } else {
-            displayName = Texts[i].name;
-            }
 
-            await knex('text').where('uuid', Texts[i].uuid)
-            .update('display_name', displayName);
-
-        }     
+    await knex('text')
+      .where('uuid', text.uuid)
+      .update('display_name', displayName);
+  }
+  Texts.forEach(generateDisplayName);
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    const hasTable= await knex.schema.hasTable('text');
+  const hasTable = await knex.schema.hasTable('text');
 
-    if (hasTable) {
-        const hasColumn = await knex.schema.hasColumn('text', 'display_name');
-        if (hasColumn) {
-        await knex.schema.table('text', table => {
-            table.dropColumn('display_name');
-        });
-       }
+  if (hasTable) {
+    const hasColumn = await knex.schema.hasColumn('text', 'display_name');
+    if (hasColumn) {
+      await knex.schema.table('text', table => {
+        table.dropColumn('display_name');
+      });
     }
+  }
 }
-
-
