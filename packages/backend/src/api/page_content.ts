27de8ec627f35c329b43bbd2e8da8402a7904131
Jooyeon Park <jooyeonpark0131@gmail.com>
2022-01-11 13:38:19 -0700

    Route update

--- packages/backend/src/api/page_content.ts
+++ packages/backend/src/api/page_content.ts
@@ -6,20 +6,23 @@ import adminRoute from '@/middlewares/adminRoute';
 const router = express.Router();
 
 router
-  .route(routeName)
+  .route('/page_content/:routeName')
   .get(async (req, res, next) => {
     try {
+      const { routeName } = req.params;
       const PageContentDao = sl.get('PageContentDao');
       const content = await PageContentDao.getContent(routeName);
-      return content;
+      res.json(content);
     } catch (err) {
       next(new HttpInternalError(err));
     }
   })
   .patch(adminRoute, async (req, res, next) => {
     try {
+      const { routeName } = req.params;
+      const { content } = req.body;
       const PageContentDao = sl.get('PageContentDao');
-      await PageContentDao.editContent(routeName, newContent);
+      await PageContentDao.editContent(routeName, content);
       res.status(204).end();
     } catch (err) {
       next(new HttpInternalError(err));
