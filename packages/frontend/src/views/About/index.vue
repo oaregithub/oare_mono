0fe6c90dd227c17af2aaf001e9f2f9eff908246c
Jooyeon Park <jooyeonpark0131@gmail.com>
2022-02-15 12:46:53 -0700

    button added, not functionaing yet

--- packages/frontend/src/views/About/index.vue
+++ packages/frontend/src/views/About/index.vue
@@ -1,19 +1,29 @@
 <template>
   <OareContentView title="About">
     <div class="title font-weight-regular">
+      <v-btn v-if="isAdmin" depressed color="primary">Edit</v-btn>
+      <br />
       <page-content pageName="about" />
     </div>
   </OareContentView>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, computed } from '@vue/composition-api';
 import PageContent from '@/components/base/OarePageContent.vue';
+import sl from '@/serviceLocator';
 
 export default defineComponent({
   components: {
     PageContent,
   },
-  setup() {},
+  setup() {
+    const store = sl.get('store');
+    const isAdmin = computed(() => store.getters.isAdmin);
+
+    return {
+      isAdmin,
+    };
+  },
 });
 </script>
--- packages/frontend/src/views/Home/LandingPage.vue
+++ packages/frontend/src/views/Home/LandingPage.vue
@@ -2,6 +2,8 @@
   <OareContentView>
     <v-row justify="center" class="title font-weight-regular">
       <v-col cols="12" lg="10">
+        <v-btn v-if="isAdmin" depressed color="primary" v-on="edit">Edit</v-btn>
+        <br />
         <page-content pageName="home" />
       </v-col>
     </v-row>
@@ -9,13 +11,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, computed } from '@vue/composition-api';
 import PageContent from '@/components/base/OarePageContent.vue';
+import sl from '@/serviceLocator';
 
 export default defineComponent({
   components: {
     PageContent,
   },
-  setup() {},
+  setup() {
+    const store = sl.get('store');
+    const isAdmin = computed(() => store.getters.isAdmin);
+
+    return {
+      isAdmin,
+    };
+  },
+  methods: {
+    edit() {},
+  },
 });
 </script>
