174e05b69f159a899467ed682622021755c05fda
Jooyeon Park <jooyeonpark0131@gmail.com>
2022-02-16 10:13:33 -0700

    Page content editor applied, but having a server error(500)

--- packages/frontend/src/components/base/OarePageContent.vue
+++ packages/frontend/src/components/base/OarePageContent.vue
@@ -1,10 +1,29 @@
 <template>
-  <div v-html="content" class="title font-weight-regular"></div>
+  <div>
+    <div v-if="isAdmin">
+      <vue-editor v-if="isEditorActive" v-model="content" /><br />
+      <v-btn depressed color="primary" @click="edit">Edit</v-btn>
+      <v-btn v-if="isEditorActive" depressed color="normal" @click="cancel"
+        >Cancel</v-btn
+      >
+    </div>
+    <div
+      v-if="isEditorActive == false"
+      v-html="content"
+      class="title font-weight-regular"
+    ></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from '@vue/composition-api';
+import {
+  defineComponent,
+  computed,
+  ref,
+  onMounted,
+} from '@vue/composition-api';
 import sl from '@/serviceLocator';
+import { VueEditor } from 'vue2-editor';
 
 export default defineComponent({
   props: {
@@ -13,11 +32,17 @@ export default defineComponent({
       required: true,
     },
   },
+  components: {
+    VueEditor,
+  },
   setup(props) {
     const server = sl.get('serverProxy');
+    const store = sl.get('store');
     const actions = sl.get('globalActions');
+    const isAdmin = computed(() => store.getters.isAdmin);
 
     const content = ref('');
+    const isEditorActive = ref(false);
 
     onMounted(async () => {
       try {
@@ -28,8 +53,25 @@ export default defineComponent({
         );
       }
     });
+
+    const edit = async () => {
+      console.log(props.pageName);
+      console.log(content.value);
+      if (isEditorActive.value) {
+        await server.updatePageContent(props.pageName, content.value);
+      }
+      isEditorActive.value = !isEditorActive.value;
+    };
+
+    const cancel = async () => {
+      isEditorActive.value = false;
+    };
     return {
       content,
+      isAdmin,
+      isEditorActive,
+      edit,
+      cancel,
     };
   },
 });
--- packages/frontend/src/views/Home/LandingPage.vue
+++ packages/frontend/src/views/Home/LandingPage.vue
@@ -2,11 +2,6 @@
   <OareContentView>
     <v-row justify="center" class="title font-weight-regular">
       <v-col cols="12" lg="10">
-<<<<<<< HEAD
-        <v-btn v-if="isAdmin" depressed color="primary" v-on="edit">Edit</v-btn>
-        <br />
-=======
->>>>>>> master
         <page-content pageName="home" />
       </v-col>
     </v-row>
@@ -14,33 +9,15 @@
 </template>
 
 <script lang="ts">
-<<<<<<< HEAD
-import { defineComponent, computed } from '@vue/composition-api';
-import PageContent from '@/components/base/OarePageContent.vue';
-import sl from '@/serviceLocator';
-=======
 import { defineComponent } from '@vue/composition-api';
 import PageContent from '@/components/base/OarePageContent.vue';
->>>>>>> master
 
 export default defineComponent({
   components: {
     PageContent,
   },
-<<<<<<< HEAD
   setup() {
-    const store = sl.get('store');
-    const isAdmin = computed(() => store.getters.isAdmin);
-
-    return {
-      isAdmin,
-    };
+    return {};
   },
-  methods: {
-    edit() {},
-  },
-=======
-  setup() {},
->>>>>>> master
 });
 </script>
