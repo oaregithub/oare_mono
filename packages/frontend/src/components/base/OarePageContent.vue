e568e284e345a6499ca54ee7fbdc06c70635b9dd
Jooyeon Park <jooyeonpark0131@gmail.com>
2022-02-25 11:52:45 -0700

    PageContent test done

--- packages/frontend/src/components/base/OarePageContent.vue
+++ packages/frontend/src/components/base/OarePageContent.vue
@@ -3,7 +3,11 @@
     <div v-if="isAdmin">
       <vue-editor v-if="isEditorActive" v-model="content" /><br />
 
-      <v-btn class="mr-2" v-if="isEditorActive" color="primary" @click="edit"
+      <v-btn
+        class="mr-2 test-save-button"
+        v-if="isEditorActive"
+        color="primary"
+        @click="edit"
         >Save</v-btn
       >
       <v-btn v-if="isEditorActive" text @click="cancel">Cancel</v-btn>
@@ -13,9 +17,13 @@
       v-html="content"
       class="title font-weight-regular"
     ></div>
-    <v-row
+    <v-row v-if="isAdmin"
       ><v-spacer />
-      <v-btn v-if="isEditorActive == false" color="primary" @click="edit"
+      <v-btn
+        class="test-edit-button"
+        v-if="isEditorActive == false"
+        color="primary"
+        @click="edit"
         >Edit</v-btn
       ></v-row
     >
