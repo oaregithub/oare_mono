d7feea8033776572d98c565a890726515b5c03f8
10808249 <jhjun98@gmail.com>
2022-03-03 13:32:09 -0700

    fixed TextSourceReading (2)

--- packages/frontend/src/views/Texts/EpigraphyView/EpigraphyDisplay/components/TextSourceReading.vue
+++ packages/frontend/src/views/Texts/EpigraphyView/EpigraphyDisplay/components/TextSourceReading.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h1>TEXT SOURCE</h1>
-    <div>{{getTextLinksByTextUuid()}}</div>
+    <div v-for="link in linksList">
+      <span v-html="readTextFile(link)"></span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, PropType, computed } from '@vue/composition-api';
-import { DiscourseUnit, EpigraphicUnitSide } from '@oare/types';
+import { defineComponent, ref, onMounted, PropType, computed } from '@vue/composition-api';
 import sl from '@/serviceLocator';
 
 export default defineComponent({
@@ -17,22 +18,37 @@ export default defineComponent({
       required: true,
     },
   },
-  setup({ discourseUnits, textUuid }) {
+  setup({ textUuid }) {
     const server = sl.get('serverProxy');
     const actions = sl.get('globalActions');
+    const linksList = ref([]);
+    const linksText = ref([]);
+    const reader = new FileReader();
 
-    const getTextLinksByTextUuid = async () => {
+    onMounted(async () => {
       try {
-        const data = await server.getTextLinksByTextUuid(textUuid);
-        console.log(data);
+        linksList.value = await server.getTextLinksByTextUuid(textUuid);
       } catch (err) {
         actions.showErrorSnackbar('Failed to get text data', err as Error);
       }
-      return data;
+    });
+
+    const readTextFile = (link: string) => {
+      /*
+      try {
+        linksText.value += await server.getTextLinksByTextUuid(textUuid);
+      } catch (err) {
+        actions.showErrorSnackbar('Failed to read text', err as Error);
+      }
+      */
+      //const response = server.getTextFileByLink(link);
+      //console.log(response);
+      return link;
     };
 
     return {
-        getTextLinksByTextUuid,
+        linksList,
+        readTextFile,
     };
   },
 });
