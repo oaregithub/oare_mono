07b4d9d5a6fa654b13de2258c3a0b65761049f4d
edstratford <edstratford@gmail.com>
2022-03-24 07:53:10 -0600

    second try - with FK relationships included - don't know if hasFK variables can be better configured to check for FK relationship instead of mere existence

--- packages/backend/migrations/20220323153708_add_tree_table.ts
+++ packages/backend/migrations/20220323153708_add_tree_table.ts
@@ -7,12 +7,54 @@ export async function up(knex: Knex): Promise<void> {
       table.charset('utf8mb4');
       table.collate('utf8mb4_bin');
       table.increments('id').primary();
-      table.uuid('uuid').notNullable().unique();
+      table
+        .uuid('uuid')
+        .notNullable()
+        .unique()
+        .references('uuid')
+        .inTable('uuid');
       table.string('type', 6);
+    });
+  }
+  const hasFKepigraphy = await knex.schema.hasColumn(
+    'text_epigraphy',
+    'tree_uuid'
+  );
+  if (hasFKepigraphy) {
+    await knex.schema.alterTable('text_epigraphy', table => {
+      table.foreign('tree_uuid').references('uuid').inTable('uuid');
+    });
+  }
+  const hasFKdiscourse = await knex.schema.hasColumn(
+    'text_discourse',
+    'tree_uuid'
+  );
+  if (hasFKdiscourse) {
+    await knex.schema.alterTable('text_discourse', table => {
+      table.foreign('tree_uuid').references('uuid').inTable('uuid');
     });
   }
 }
 export async function down(knex: Knex): Promise<void> {
+  const hasFKepigraphy = await knex.schema.hasColumn(
+    'text_epigraphy',
+    'tree_uuid'
+  );
+  if (hasFKepigraphy) {
+    await knex.schema.alterTable('text_epigraphy', table => {
+      table.dropForeign('tree_uuid');
+    });
+  }
+  const hasFKdiscourse = await knex.schema.hasColumn(
+    'text_discourse',
+    'tree_uuid'
+  );
+  if (hasFKdiscourse) {
+    await knex.schema.alterTable('text_discourse', table => {
+      table.dropForeign('tree_uuid');
+    });
+  }
+
   const hasTreeTable = await knex.schema.hasTable('tree');
   if (hasTreeTable) {
     await knex.schema.dropTable('tree');
