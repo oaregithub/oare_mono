4a458e7144ced86c367002cf488c426e9efca48c
10808249 <jhjun98@gmail.com>
2022-03-29 12:52:17 -0600

    created empty text info card

---
+++ packages/frontend/src/views/Texts/EpigraphyView/EpigraphyDisplay/components/TextSourceInformationCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-menu offset-y open-on-hover>
+    <template #activator="{ on, attrs }">
+      <v-icon v-bind="attrs" v-on="on" class="mb-1 ml-1">
+        mdi-information-outline
+      </v-icon>
+    </template>
+    <v-card class="pa-3">
+      Hello, world!
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
+
+<style></style>
