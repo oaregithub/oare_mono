9cbc020f3da3ac9bfb631ed538439b2c5b2ea3b0
Harrison Bludworth <harrisonbludworth@harrisons-mbp-2.lan>
2022-04-22 11:04:08 -0600

    added info statements and progress bar

--- packages/backend/migrations/20220415100541_create_tree_table.ts
+++ packages/backend/migrations/20220415100541_create_tree_table.ts
@@ -7,10 +7,7 @@ const discourseTreeFK = 'discourse_tree_uuid_foreign';
 export async function up(knex: Knex): Promise<void> {
   const hasTreeTable = await knex.schema.hasTable('tree');
   if (!hasTreeTable) {
-    const progressBar1 = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
+    console.info('1/6 - Generating list of existing Tree UUIDs');
 
     const existingEpigraphyTreeUuids: string[] = [
       ...new Set(await knex('text_epigraphy').pluck('tree_uuid')),
@@ -30,16 +27,24 @@ export async function up(knex: Knex): Promise<void> {
       table_reference: 'tree',
     }));
 
-    progressBar1.start(allTreeUuidsInsert.length, 0);
+    console.info('2/6 - Adding Tree UUIDs to uuid table');
 
-    await Promise.all(
-      allTreeUuidsInsert.map(row => {
-        progressBar1.increment();
-        return knex('uuid').insert(row);
-      })
+    const progressBar1 = new cliProgress.SingleBar(
+      {},
+      cliProgress.Presets.shades_classic
     );
 
+    progressBar1.start(allTreeUuidsInsert.length, 0);
+
+    for (let i = 0; i < allTreeUuidsInsert.length; i += 1) {
+      progressBar1.increment();
+      // eslint-disable-next-line no-await-in-loop
+      await knex('uuid').insert(allTreeUuidsInsert[i]);
+    }
+
     progressBar1.stop();
+
+    console.info('3/6 - Creating tree table');
 
     await knex.schema.createTable('tree', table => {
       table.charset('utf8mb4');
@@ -71,6 +76,8 @@ export async function up(knex: Knex): Promise<void> {
       ...existingDiscourseTreeInserts,
     ];
 
+    console.info('4/6 - Inserting existing Tree UUIDs into tree table');
+
     const progressBar2 = new cliProgress.SingleBar(
       {},
       cliProgress.Presets.shades_classic
@@ -78,38 +85,47 @@ export async function up(knex: Knex): Promise<void> {
 
     progressBar2.start(allExistingRows.length, 0);
 
-    await Promise.all(
-      allExistingRows.map(row => {
-        progressBar2.increment();
-        return knex('tree').insert(row);
-      })
-    );
+    for (let i = 0; i < allExistingRows.length; i += 1) {
+      progressBar2.increment();
+      // eslint-disable-next-line no-await-in-loop
+      await knex('tree').insert(allExistingRows[i]);
+    }
 
     progressBar2.stop();
+
+    console.info(
+      '5/6 - Creating foreign key relationship in text_epigraphy table - Est. 1-2 hours'
+    );
 
     await knex.schema.alterTable('text_epigraphy', table => {
       table.foreign('tree_uuid', epigraphyTreeFK).references('tree.uuid');
     });
 
+    console.info(
+      '6/6 - Creating foreign key relationship in text_discourse table - Est. 1-2 hours'
+    );
+
     await knex.schema.alterTable('text_discourse', table => {
       table.foreign('tree_uuid', discourseTreeFK).references('tree.uuid');
     });
+
+    console.info('Migration Complete');
   }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('text_epigraphy', table => {
-    table.dropForeign(['tree_uuid'], epigraphyTreeFK);
-  });
-
-  await knex.schema.alterTable('text_discourse', table => {
-    table.dropForeign(['tree_uuid'], discourseTreeFK);
-  });
-
   const hasTreeTable = await knex.schema.hasTable('tree');
   if (hasTreeTable) {
-    await knex.schema.dropTable('tree');
-  }
+    await knex.schema.alterTable('text_epigraphy', table => {
+      table.dropForeign(['tree_uuid'], epigraphyTreeFK);
+    });
 
-  await knex('uuid').del().where('table_reference', 'tree');
+    await knex.schema.alterTable('text_discourse', table => {
+      table.dropForeign(['tree_uuid'], discourseTreeFK);
+    });
+
+    await knex.schema.dropTable('tree');
+
+    await knex('uuid').del().where('table_reference', 'tree');
+  }
 }
