b774aa6ef57206e57cd4b151b72cebb1417cb368
hbludworth <63749412+hbludworth@users.noreply.github.com>
2022-04-28 19:39:24 -0600

    added .sh file for zip script (#1278)
    Co-authored-by: Harrison Bludworth <harrisonbludworth@harrisons-mbp-2.lan>

---
+++ zip.sh
@@ -0,0 +1,12 @@
+cd packages/backend
+zip -q -r oare_build.zip build dist package.json yarn.lock @oare
+mv oare_build.zip ../..
+cd ../..
+mkdir oare_build
+unzip oare_build.zip -d ./oare_build
+cd oare_build
+yarn install
+zip -q -r oare_build.zip build dist package.json yarn.lock @oare node_modules
+mv oare_build.zip ../
+cd ..
+rm -rf oare_build
