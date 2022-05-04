e16b872477097fd4fd051e0bb9747f3c0391409e
hbludworth <63749412+hbludworth@users.noreply.github.com>
2022-05-04 12:50:10 -0600

    allows admins to view environment info (#1284)

---
+++ packages/backend/src/api/environment.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { HttpInternalError } from '@/exceptions';
+import adminRoute from '@/middlewares/adminRoute';
+import { EnvironmentInfo } from '@oare/types';
+
+const router = express.Router();
+
+router.route('/environment_info').get(adminRoute, async (_req, res, next) => {
+  try {
+    const environmentInfo: EnvironmentInfo = {
+      elasticBeanstalkRegion: process.env.AWS_REGION,
+      databaseReadRegion: 'Coming Soon',
+      databaseWriteRegion: 'Coming Soon',
+    };
+    res.json(environmentInfo);
+  } catch (err) {
+    next(new HttpInternalError(err as string));
+  }
+});
+
+export default router;
