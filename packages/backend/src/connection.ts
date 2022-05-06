4736bce04349d0595cee3a7dd397a70e75f74738
Harrison Bludworth <harrisonbludworth@harrisons-mbp-2.lan>
2022-05-06 01:36:33 -0600

    fixed linting

--- packages/backend/src/connection.ts
+++ packages/backend/src/connection.ts
@@ -5,29 +5,27 @@ type ProductionUrlMode = 'read' | 'write' | 'migration';
 const getProductionUrl = (mode: ProductionUrlMode) => {
   if (mode === 'read') {
     return process.env.OARE_DB_URL_READ;
-  } else if (mode === 'write') {
-    return process.env.OARE_DB_URL_WRITE;
-  } else {
-    return process.env.OARE_DB_URL;
   }
+  if (mode === 'write') {
+    return process.env.OARE_DB_URL_WRITE;
+  }
+  return process.env.OARE_DB_URL;
 };
 
-export const knexConfig = (mode: ProductionUrlMode): Knex.Config => {
-  return {
-    client: 'mysql',
-    connection:
-      process.env.NODE_ENV === 'production'
-        ? getProductionUrl(mode)
-        : {
-            host: 'localhost',
-            user: 'root',
-            port: 3306,
-            password: 'example',
-            database: 'oarebyue_0.3',
-          },
-    pool: { min: 0, max: 10 },
-  };
-};
+export const knexConfig = (mode: ProductionUrlMode): Knex.Config => ({
+  client: 'mysql',
+  connection:
+    process.env.NODE_ENV === 'production'
+      ? getProductionUrl(mode)
+      : {
+          host: 'localhost',
+          user: 'root',
+          port: 3306,
+          password: 'example',
+          database: 'oarebyue_0.3',
+        },
+  pool: { min: 0, max: 10 },
+});
 const knexWriteInstance = knex(knexConfig('write'));
 const knexReadInstance = knex(knexConfig('read'));
 
@@ -41,7 +39,6 @@ export const knexWrite = () => {
 export const knexRead = () => {
   if (lastWrite && Date.now() < lastWrite + 45000) {
     return knexWriteInstance;
-  } else {
-    return knexReadInstance;
   }
+  return knexReadInstance;
 };
