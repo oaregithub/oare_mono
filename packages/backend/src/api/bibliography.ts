6dbc649fa53236625cc3ff8ab4937e365d44701a
10808249 <jhjun98@gmail.com>
2022-05-30 22:04:34 -0600

    get query of zotero

--- packages/backend/src/api/bibliography.ts
+++ packages/backend/src/api/bibliography.ts
@@ -4,17 +4,25 @@ import { HttpInternalError } from '@/exceptions';
 
 const router = express.Router();
 
-router.route('/bibliography').get(async (req, res, next) => {
+router.route('/bibliography/query').get(async (req, res, next) => {
   try {
+    const pagination = (req.query.pagination || 25) as number;
     const BibliographyDao = sl.get('BibliographyDao');
-    const rows = BibliographyDao.queryAllRows();
+    const rows = await BibliographyDao.queryAllRows(); // TODO: pagination
+
+    rows.forEach(async row => {
+      await BibliographyDao.queryZotero(
+        row.zot_item_key,
+        'chicago-author-date'
+      );
+    });
   } catch (err) {
     next(new HttpInternalError(err as string));
   }
 });
 
-router.route('/bibliography/:style').get(async (req, res, next) => {
-  const style = req.params.style as string;
+router.route('/bibliography/style').get(async (req, res, next) => {
+  const style = req.query.style || 'chicago-author-date';
 });
 
 export default router;
--- packages/backend/src/api/daos/BibliographyDao/index.ts
+++ packages/backend/src/api/daos/BibliographyDao/index.ts
@@ -1,13 +1,24 @@
 import { knexRead, knexWrite } from '@/connection';
 import { BibliographySimpleRow } from '@oare/types';
+import { dynamicImport } from 'tsimportlib';
 
 class BibliographyDao {
   async queryAllRows(): Promise<BibliographySimpleRow[]> {
     const rows: BibliographySimpleRow[] = await knexRead()(
       'bibliography'
     ).select('uuid', 'zot_item_key');
-
     return rows;
+  }
+
+  async queryZotero(zot_item_key: string, style: string) {
+    const zoteroLink = `https://api.zotero.org/groups/318265/items/${zot_item_key}?format=json&include=bib,data&style=${style}`;
+
+    const fetch = (await dynamicImport(
+      'node-fetch',
+      module
+    )) as typeof import('node-fetch');
+
+    const response = await fetch.default(zoteroLink);
   }
 }
 
