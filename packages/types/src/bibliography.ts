5838a5e3bd7aaf20897f627dd09d2c5b4665c89c
10808249 <jhjun98@gmail.com>
2022-06-02 12:48:53 -0600

    created zotero response type

--- packages/types/src/bibliography.ts
+++ packages/types/src/bibliography.ts
@@ -1,5 +1,40 @@
-export interface bibliography {
+export interface Bibliography {
   uuid: string;
   zoteroKey: string;
   citation: string;
+}
+
+export interface ZoteroResponse {
+  key: string;
+  version: number;
+  library: {
+    type: string;
+    id: number;
+    name: string;
+    links: {
+      alternate: ZoteroLink;
+    };
+  };
+  links: {
+    self: ZoteroLink;
+    alternate: ZoteroLink;
+  };
+  meta: {
+    createdByUser: {
+      id: number;
+      username: string;
+      name: string;
+      links: {
+        alternate: ZoteroLink;
+      };
+      creatorSummary: string;
+      numChildren: number;
+    };
+  };
+  bib: string;
+}
+
+export interface ZoteroLink {
+  href: string;
+  type: string;
 }
