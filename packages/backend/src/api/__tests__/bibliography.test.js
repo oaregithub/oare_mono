74cac4432ec1009556709fa214ecf58d95d70004
10808249 <jhjun98@gmail.com>
2022-07-02 17:44:58 -0600

    fixed test & types

--- packages/backend/src/api/__tests__/bibliography.test.js
+++ packages/backend/src/api/__tests__/bibliography.test.js
@@ -4,36 +4,100 @@ import request from 'supertest';
 import sl from '@/serviceLocator';
 
 describe('GET /bibliography/query', () => {
-  const mockBibliographyDao = {
+  const PATH = `${API_PATH}/bibliography/query`;
 
+  const mockBibliographyDao = {
+    queryBibliographyByPage: jest.fn().mockResolvedValue([
+      {
+        uuid: 'bib-test-uuid',
+      },
+    ]),
+  };
+
+  const mockBibliographyUtils = {
+    fetchZotero: jest.fn().mockResolvedValue([
+      {
+        bib: 'mock-bib-3',
+        data: {
+          title: 'Hello',
+        },
+      },
+    ]),
+  };
+
+  const mockResourceDao = {
+    getFileURLByUuid: jest
+      .fn()
+      .mockResolvedValue([
+        'https://oare-unit-test.com/test-resource-link-def.pdf',
+      ]),
+  };
+
+  const mockResponse = {
+    bibs: ['mock-bib-3'],
+    datas: [
+      {
+        title: 'Hello',
+      },
+    ],
+    fileURL: ['https://oare-unit-test.com/test-resource-link-def.pdf'],
+  };
+
+  const mockUserDao = {
+    getUserByUuid: jest.fn().mockResolvedValue({
+      isAdmin: true,
+    }),
+  };
+
+  const mockPermissionsDao = {
+    getUserPermissions: jest.fn().mockResolvedValue([
+      {
+        name: 'VIEW_BIBLIOGRAPHY',
+      },
+    ]),
   };
 
   const setup = () => {
     sl.set('BibliographyDao', mockBibliographyDao);
+    sl.set('BibliographyUtils', mockBibliographyUtils);
+    sl.set('ResourceDao', mockResourceDao);
+    sl.set('UserDao', mockUserDao);
+    sl.set('PermissionsDao', mockPermissionsDao);
   };
 
   beforeEach(setup);
 
-  // const sendRequest = () => request(app).get(PATH);
+  const sendRequest = () =>
+    request(app).get(PATH).set('Authorization', 'token');
 
-  it('TODO /bibliography/query', async () => {
-    expect('ABC').toBe('ABC');
+  it('returns 200 on successful bibliography request', async () => {
+    const response = await sendRequest();
+    expect(response.status).toBe(200);
+    expect(JSON.parse(response.text)).toEqual(mockResponse);
   });
-});
 
-describe('GET /bibliography/style', () => {
-  const mockBibliographyDao = {
+  it('returns 401 if user is not logged in', async () => {
+    const response = await request(app).get(PATH);
+    expect(response.status).toBe(401);
+  });
 
-  };
+  it('returns 403 if user does not have permission', async () => {
+    sl.set('PermissionsDao', {
+      ...mockPermissionsDao,
+      getUserPermissions: jest.fn().mockResolvedValue([]),
+    });
+    const response = await sendRequest();
+    expect(response.status).toBe(403);
+  });
 
-  const setup = () => {
-  };
-
-  beforeEach(setup);
-
-  // const sendRequest = () => request(app).get(PATH);
-
-  it('TODO /bibliography/style', async () => {
-    expect('ABC').toBe('ABC');
+  it('returns 500 on failed fetching bibliography', async () => {
+    sl.set('BibliographyDao', {
+      ...mockBibliographyDao,
+      queryBibliographyByPage: jest
+        .fn()
+        .mockRejectedValue('failed to retreive bibliography'),
+    });
+    const response = await sendRequest();
+    expect(response.status).toBe(500);
   });
 });
--- packages/backend/src/api/bibliography.ts
+++ packages/backend/src/api/bibliography.ts
@@ -2,79 +2,59 @@ import express from 'express';
 import sl from '@/serviceLocator';
 import { HttpInternalError } from '@/exceptions';
 import { extractPagination } from '@/utils';
+import { BibliographyResponse } from '@oare/types';
+import permissionsRoute from '@/middlewares/permissionsRoute';
 
 const router = express.Router();
 
-router.route('/bibliography/query').get(async (req, res, next) => {
-  try {
-    const { filter: search, limit: rows, page } = extractPagination(req.query, {
-      defaultLimit: 25,
-    });
-    const BibliographyDao = sl.get('BibliographyDao');
-    const BibliographyUtils = sl.get('BibliographyUtils');
-    const ResourceDao = sl.get('ResourceDao');
-    const citationStyle = 'chicago-author-date';
+router
+  .route('/bibliography/query')
+  .get(permissionsRoute('VIEW_BIBLIOGRAPHY'), async (req, res, next) => {
+    try {
+      const { filter: search, limit: rows, page } = extractPagination(
+        req.query,
+        {
+          defaultLimit: 25,
+        }
+      );
+      const BibliographyDao = sl.get('BibliographyDao');
+      const BibliographyUtils = sl.get('BibliographyUtils');
+      const ResourceDao = sl.get('ResourceDao');
+      const citationStyle = (req.query.style ||
+        'chicago-author-date') as string;
 
-    const bibliographies = await BibliographyDao.queryBibliographyByPage({
-      page,
-      rows,
-    });
+      const bibliographies = await BibliographyDao.queryBibliographyByPage({
+        page,
+        rows,
+      });
 
-    const zoteroResponse = await BibliographyUtils.fetchZotero(
-      bibliographies,
-      citationStyle
-    );
+      const zoteroResponse = await BibliographyUtils.fetchZotero(
+        bibliographies,
+        citationStyle
+      );
 
-    const objUuids = bibliographies.map(item => item.uuid);
+      const bibs = zoteroResponse
+        .filter(item => !!item.bib)
+        .map(item => item.bib!);
 
-    const fileURL = await ResourceDao.getFileURLByUuid(objUuids);
+      const datas = zoteroResponse
+        .filter(item => !!item.data)
+        .map(item => item.data!);
 
-    const bibs = zoteroResponse.map(item => item.bib);
+      const objUuids = bibliographies.map(item => item.uuid);
 
-    const datas = zoteroResponse.map(item => item.data);
+      const fileURL = await ResourceDao.getFileURLByUuid(objUuids);
 
-    const response = { bibs, datas, fileURL };
+      const response: BibliographyResponse = {
+        bibs,
+        datas,
+        fileURL,
+      };
 
-    res.json(response);
-  } catch (err) {
-    next(new HttpInternalError(err as string));
-  }
-});
-
-router.route('/bibliography/style').get(async (req, res, next) => {
-  try {
-    const { filter: search, limit: rows, page } = extractPagination(req.query, {
-      defaultLimit: 25,
-    });
-    const BibliographyDao = sl.get('BibliographyDao');
-    const BibliographyUtils = sl.get('BibliographyUtils');
-    const ResourceDao = sl.get('ResourceDao');
-    const citationStyle = (req.query.style || 'chicago-author-date') as string;
-
-    const bibliographies = await BibliographyDao.queryBibliographyByPage({
-      page,
-      rows,
-    });
-
-    const zoteroResponse = await BibliographyUtils.fetchZotero(
-      bibliographies,
-      citationStyle
-    );
-
-    const objUuids = bibliographies.map(item => item.uuid);
-
-    const fileURL = await ResourceDao.getFileURLByUuid(objUuids);
-
-    const bibs = zoteroResponse.map(item => item.bib);
-
-    const datas = zoteroResponse.map(item => item.data);
-
-    const response = { bibs, datas, fileURL };
-
-    res.json(response);
-  } catch (err) {
-    next(new HttpInternalError(err as string));
-  }
-});
+      res.json(response);
+    } catch (err) {
+      next(new HttpInternalError(err as string));
+    }
+  });
 
 export default router;
