5c0a2b660441db27eef4f7ba99d6261311a2fedd
10808249 <jhjun98@gmail.com>
2022-07-26 10:46:07 -0600

    added radio button group (on develop)

--- packages/frontend/src/views/Bibliography/index.vue
+++ packages/frontend/src/views/Bibliography/index.vue
@@ -1,21 +1,72 @@
 <template>
   <OareContentView title="Bibliography">
     <div class="title font-weight-regular">
-      <page-content pageName="bibliography" />
+      <v-row>
+        <v-col cols="2">
+          <v-radio-group v-model="selectedType">
+            <v-radio
+              v-for="(type, idx) in types"
+              :key="idx"
+              :label="type.name"
+              :value="type.value"
+              class="test-radio-btn"
+            ></v-radio>
+          </v-radio-group>
+        </v-col>
+        <v-col cols="10">
+          <text-collection-list
+            v-if="selectedType === 'text'"
+            :key="selectedType"
+            :groupId="groupId"
+            itemType="Text"
+            :getItems="server.getGroupAllowlistTexts"
+            :removeItems="server.removeItemsFromGroupAllowlist"
+          />
+          <text-collection-list
+            v-else-if="selectedType === 'collection'"
+            :key="selectedType"
+            :groupId="groupId"
+            itemType="Collection"
+            :getItems="server.getGroupAllowlistCollections"
+            :removeItems="server.removeItemsFromGroupAllowlist"
+          />
+        </v-col>
+      </v-row>
     </div>
   </OareContentView>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 import PageContent from '@/components/base/OarePageContent.vue';
+import TextCollectionList from '@/views/Admin/components/TextCollectionList.vue';
+import useQueryParam from '@/hooks/useQueryParam';
+import sl from '@/serviceLocator';
 
 export default defineComponent({
   components: {
+    TextCollectionList,
     PageContent,
   },
+  props: {
+    groupId: {
+      type: String,
+      required: true,
+    },
+  },
   setup() {
-    return {};
+    const server = sl.get('serverProxy');
+    const types = ref([
+      { name: 'Texts', value: 'text' },
+      { name: 'Collections', value: 'collection' },
+    ]);
+    const selectedType = useQueryParam('type', 'text', true);
+
+    return {
+      server,
+      types,
+      selectedType,
+    };
   },
 });
 </script>
