686b4e490ba26a24b1240ab262b2ad82e1a7aade
justisbrown <64491880+justisbrown@users.noreply.github.com>
2022-08-26 06:53:02 -0600

    fixes test failure

--- packages/frontend/src/views/Signs/__tests__/SignList.test.js
+++ packages/frontend/src/views/Signs/__tests__/SignList.test.js
@@ -73,7 +73,7 @@ describe('SignList test', () => {
   it('gets signs when sortBy is changed', async () => {
     const wrapper = createWrapper();
     await flushPromises();
-    const sortByOptions = wrapper.find('.test-checkbox-abz');
+    const sortByOptions = wrapper.find('.test-checkbox-ABZ');
     await sortByOptions.trigger('click');
     await flushPromises();
     expect(mockServer.getSignList).toHaveBeenCalled();
@@ -87,7 +87,7 @@ describe('SignList test', () => {
       },
     });
     await flushPromises();
-    const sortByOptions = wrapper.find('.test-checkbox-abz');
+    const sortByOptions = wrapper.find('.test-checkbox-ABZ');
     await sortByOptions.trigger('click');
     await flushPromises();
     expect(mockActions.showErrorSnackbar).toHaveBeenCalled();
