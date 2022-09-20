711b6872191eef148676bf6b37577bf7a4a59922
edstratford <edstratford@gmail.com>
2022-09-19 20:55:02 -0600

    minor changes

--- packages/frontend/src/views/Texts/EpigraphyView/EpigraphyDisplay/__tests__/EpigraphyFullDisplay.test.js
+++ packages/frontend/src/views/Texts/EpigraphyView/EpigraphyDisplay/__tests__/EpigraphyFullDisplay.test.js
@@ -171,7 +171,7 @@ describe('EpigraphyFullDisplay View', () => {
       },
     });
     await flushPromises();
-    await wrapper.get('.test-epigraphies');
+    await wrapper.get('.test-rendered-word').at(0).trigger('click');
     await flushPromises();
     await wrapper.get('.test-disconnect-word').trigger('click)');
     await flushPromises();
