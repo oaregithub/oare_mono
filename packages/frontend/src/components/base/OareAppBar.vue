754cedc0a9cd181b60b810e6626c3f601dd41fbb
justisbrown <64491880+justisbrown@users.noreply.github.com>
2022-10-24 11:33:11 -0600

    adds nav button for seals

--- packages/frontend/src/components/base/OareAppBar.vue
+++ packages/frontend/src/components/base/OareAppBar.vue
@@ -192,6 +192,9 @@
             <v-list-item class="pa-0">
               <v-btn text to="/signList" width="100%">Sign List</v-btn>
             </v-list-item>
+            <v-list-item class="pa-0">
+              <v-btn text to="/seals" width="100%">Seals</v-btn>
+            </v-list-item>
           </v-list>
         </v-menu>
       </v-row>
