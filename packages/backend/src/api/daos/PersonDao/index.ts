f788934dd0afc18211d8ba34812a1c1fe676ed1a
edstratford <edstratford@gmail.com>
2022-10-25 10:25:14 -0600

    dao, initial router (#1504)
    * dao, initial router
    
    * minor edits
    
    * commented test out
    
    * resolved requested changes

--- packages/backend/src/api/daos/PersonDao/index.ts
+++ packages/backend/src/api/daos/PersonDao/index.ts
@@ -1,5 +1,5 @@
 import { knexRead } from '@/connection';
-import { PersonDisplay } from '@oare/types';
+import { PersonDisplay, PersonRow } from '@oare/types';
 import { Knex } from 'knex';
 import utils from '../utils';
 
@@ -73,6 +73,61 @@ class PersonDao {
       .orderBy('word');
 
     return people;
+  }
+
+  async getPersonsRowsByLetter(
+    letter: string,
+    trx?: Knex.Transaction
+  ): Promise<PersonRow[]> {
+    const k = trx || knexRead();
+    const letters = letter.split('/');
+    let query = k('person')
+      .select(
+        'person.uuid',
+        'name_uuid AS nameUuid',
+        'relation',
+        'relation_name_uuid AS relationNameUuid',
+        'label',
+        'person.type'
+      )
+      .leftJoin('dictionary_word', 'person.name_uuid', 'dictionary_word.uuid')
+      .where('person.type', 'person');
+
+    letters.forEach(possibleVowel => {
+      switch (possibleVowel) {
+        case 'a':
+          return letters.push('ā');
+        case 'e':
+          return letters.push('ē');
+        case 'i':
+          return letters.push('ī');
+        case 'o':
+          return letters.push('ō');
+        case 'u':
+          return letters.push('ū');
+        default:
+          return letters;
+      }
+    });
+
+    query = query.andWhere(qb => {
+      letters.forEach(l => {
+        qb.orWhere('word', 'like', `${l.toLocaleUpperCase()}%`)
+          .orWhere('word', 'like', `(${l.toLocaleUpperCase()}%`)
+          .orWhere('word', 'like', `${l.toLocaleLowerCase()}%`)
+          .orWhere('word', 'like', `(${l.toLocaleLowerCase()}%`)
+          .orWhere('word', null)
+          .andWhere(qb1 => {
+            qb1
+              .orWhere('label', 'like', `${l.toLocaleUpperCase()}%`)
+              .orWhere('label', 'like', `(${l.toLocaleUpperCase()}%`)
+              .orWhere('label', 'like', `${l.toLocaleLowerCase()}%`)
+              .orWhere('label', 'like', `(${l.toLocaleLowerCase()}%`);
+          });
+      });
+    });
+
+    return query;
   }
 }
 
