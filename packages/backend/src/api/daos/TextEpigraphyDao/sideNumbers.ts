081dd987fc5d42da1792390bdd4dd71c6de5eff1
edstratford <edstratford@gmail.com>
2023-01-18 21:00:54 -0700

    update column sidenumber list after column row use

--- packages/backend/src/api/daos/TextEpigraphyDao/sideNumbers.ts
+++ packages/backend/src/api/daos/TextEpigraphyDao/sideNumbers.ts
@@ -12,21 +12,7 @@ const sideNumbers: Record<number, EpigraphicUnitSide> = {
   9: 'suppl. tablet',
   10: 'vs.!',
   11: 'near hilt',
-  12: 'obv. col. i',
-  13: 'obv. col. ii',
-  14: 'le.e. col. i',
-  15: 'le.e. col. ii',
-  16: 'le.e. col. iii',
-  17: 'col. i',
-  18: 'col. ii',
-  19: 'col. iii',
-  20: 'col. iv',
-  21: 'col. v',
-  22: 'col. vi',
-  23: "col. i'",
-  24: "col. ii'",
-  25: "col. iii'",
-  26: "col. iv'",
+  12: 'obv.',
 };
 
 export default sideNumbers;
