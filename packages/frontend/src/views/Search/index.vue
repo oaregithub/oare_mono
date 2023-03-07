a0188a03fcd718140807698216bbe4d59ce50979
Harrison Bludworth <harrisonbludworth@gmail.com>
2023-03-07 11:31:10 -0700

    added permission check to search page

--- packages/frontend/src/views/Search/index.vue
+++ packages/frontend/src/views/Search/index.vue
@@ -3,7 +3,10 @@
     <router-link to="/search/texts" class="oare-title mr-4"
       >Text Name</router-link
     >
-    <router-link to="/search/dictionary" class="oare-title mr-4"
+    <router-link
+      to="/search/dictionary"
+      class="oare-title mr-4"
+      v-if="canDictionarySearch"
       >Dictionary</router-link
     >
     <router-link to="/search/wordsInTexts" class="oare-title"
@@ -14,10 +17,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, computed } from '@vue/composition-api';
 import TextsSearch from './TextsSearch/TextsSearch.vue';
 import WordsInTextsSearch from './TextsSearch/WordsInTextSearch.vue';
 import DictionarySearch from './DictionarySearch/DictionarySearch.vue';
+import sl from '@/serviceLocator';
 
 export default defineComponent({
   name: 'SearchView',
@@ -25,6 +29,19 @@ export default defineComponent({
     TextsSearch,
     DictionarySearch,
     WordsInTextsSearch,
+  },
+  setup() {
+    const store = sl.get('store');
+
+    const canDictionarySearch = computed(
+      () =>
+        store.hasPermission('WORDS') ||
+        store.hasPermission('NAMES') ||
+        store.hasPermission('PLACES')
+    );
+    return {
+      canDictionarySearch,
+    };
   },
 });
 </script>
