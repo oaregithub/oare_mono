8c5242a03a9c7b57526f874bb9c243dc72035bc1
edstratford <edstratford@gmail.com>
2023-03-13 15:55:54 -0600

    created Backend for individual persons

--- packages/types/src/persons.ts
+++ packages/types/src/persons.ts
@@ -1,4 +1,6 @@
 import { ItemPropertyRow } from './words';
+import { SearchTextsResultRow } from './search';
+import { FieldRow } from './field';
 
 export interface PersonRow {
   uuid: string;
@@ -6,11 +8,40 @@ export interface PersonRow {
   relation: string | null;
   relationNameUuid: string | null;
   label: string;
-  type: string;
+  descriptor: string | null;
 }
 
 export interface PersonListItem {
   person: PersonRow;
   display: string;
   properties: ItemPropertyRow[];
+}
+
+export interface PersonCore {
+  display: string;
+  nameUuid: string | null;
+  relation: string | null;
+  relationNameUuid: string | null;
+  uuid: string;
+  descriptor: string | null;
+}
+
+export interface PersonRole {
+  role: string;
+  roleUuid: string;
+}
+
+export interface PersonInfo {
+  person: PersonRow;
+  display: string;
+  father: PersonCore | null;
+  mother: PersonCore | null;
+  asshatumWives: PersonCore[];
+  amtumWives: PersonCore[];
+  husbands: PersonCore[];
+  siblings: PersonCore[];
+  children: PersonCore[];
+  durableRoles: PersonRole[];
+  discussion: FieldRow[];
+  temporaryRoles: PersonRole[];
 }
