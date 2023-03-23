158fe72789c44cac7ebc3a46f6e41ac8fe645139
edstratford <edstratford@gmail.com>
2023-03-23 14:14:22 -0600

    Adds Roles to inidividual person page

--- packages/frontend/src/views/Persons/IndivPerson.vue
+++ packages/frontend/src/views/Persons/IndivPerson.vue
@@ -1,93 +1,104 @@
 <template>
   <OareContentView :loading="loading" :title="person.display">
-    <v-row v-for="(field, idx) in person.discussion" :key="idx" class="ma-0">{{
-      field.field
-    }}</v-row>
-
-    <div v-if="person.father">
-      <b>Father: </b><span>{{ person.father.display }}</span>
-    </div>
-
-    <div v-if="person.mother">
-      <b>Mother: </b><span>{{ person.mother.display }}</span>
-    </div>
-
-    <div v-if="person.asshatumWives.length > 0">
-      <b>Aššutum Wife/Wives: </b
-      ><span v-for="(wife, idx) in person.asshatumWives" :key="idx"
-        >{{ wife.display }}
-        <span v-if="idx < person.asshatumWives.length - 1" class="ml-n1"
-          >,
-        </span></span
+    <div v-if="personHasData">
+      <v-row
+        v-for="(field, idx) in person.discussion"
+        :key="idx"
+        class="ma-0"
+        >{{ field.field }}</v-row
       >
-    </div>
 
-    <div v-if="person.amtumWives.length > 0">
-      <b>Amtum Wife/Wives: </b
-      ><span v-for="(wife, idx) in person.amtumWives" :key="idx"
-        >{{ wife.display }}
-        <span v-if="idx < person.amtumWives.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
-    </div>
+      <div v-if="person.father">
+        <b>Father: </b><span>{{ person.father.display }}</span>
+      </div>
 
-    <div v-if="person.husbands.length > 0">
-      <b>Husband(s): </b
-      ><span v-for="(husband, idx) in person.husbands" :key="idx"
-        >{{ husband.display }}
-        <span v-if="idx < person.husbands.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
-    </div>
+      <div v-if="person.mother">
+        <b>Mother: </b><span>{{ person.mother.display }}</span>
+      </div>
 
-    <div v-if="person.siblings.length > 0">
-      <b>Sibling(s): </b
-      ><span v-for="(sibling, idx) in person.siblings" :key="idx"
-        >{{ sibling.display }}
-        <span v-if="idx < person.siblings.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
-    </div>
+      <div v-if="person.asshatumWives.length > 0">
+        <b>Aššutum Wife/Wives: </b
+        ><span v-for="(wife, idx) in person.asshatumWives" :key="idx"
+          >{{ wife.display }}
+          <span v-if="idx < person.asshatumWives.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
 
-    <div v-if="person.children.length > 0">
-      <b>Children: </b
-      ><span v-for="(child, idx) in person.children" :key="idx"
-        >{{ child.display }}
-        <span v-if="idx < person.children.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
-    </div>
+      <div v-if="person.amtumWives.length > 0">
+        <b>Amtum Wife/Wives: </b
+        ><span v-for="(wife, idx) in person.amtumWives" :key="idx"
+          >{{ wife.display }}
+          <span v-if="idx < person.amtumWives.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
 
-    <div v-if="person.durableRoles.length > 0">
-      <b>Durable Roles: </b
-      ><span v-for="(role, idx) in person.durableRoles" :key="idx"
-        >{{ role.role }}
-        <span v-if="idx < person.durableRoles.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
-    </div>
+      <div v-if="person.husbands.length > 0">
+        <b>Husband(s): </b
+        ><span v-for="(husband, idx) in person.husbands" :key="idx"
+          >{{ husband.display }}
+          <span v-if="idx < person.husbands.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
 
-    <div v-if="person.temporaryRoles.length > 0">
-      <b>Other Roles: </b
-      ><span v-for="(role, idx) in person.temporaryRoles" :key="idx"
-        >{{ role.role }}
-        <span v-if="idx < person.temporaryRoles.length - 1" class="ml-n1"
-          >,
-        </span></span
-      >
+      <div v-if="person.siblings.length > 0">
+        <b>Sibling(s): </b
+        ><span v-for="(sibling, idx) in person.siblings" :key="idx"
+          >{{ sibling.display }}
+          <span v-if="idx < person.siblings.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
+
+      <div v-if="person.children.length > 0">
+        <b>Children: </b
+        ><span v-for="(child, idx) in person.children" :key="idx"
+          >{{ child.display }}
+          <span v-if="idx < person.children.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
+
+      <div v-if="personRoles.durableRoles.length > 0">
+        <b>Durable Roles: </b
+        ><span v-for="(role, idx) in personRoles.durableRoles" :key="idx"
+          >{{ role.role }} ({{ role.occurrences }})
+          <span v-if="idx < personRoles.durableRoles.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
+
+      <div v-if="personRoles.temporaryRoles.length > 0">
+        <b>Temporary Roles: </b
+        ><span v-for="(role, idx) in personRoles.temporaryRoles" :key="idx"
+          >{{ role.role }} ({{ role.occurrences }})
+          <span v-if="idx < personRoles.temporaryRoles.length - 1" class="ml-n1"
+            >,
+          </span></span
+        >
+      </div>
     </div>
+    <span v-else>There is not yet information for this person.</span>
   </OareContentView>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from '@vue/composition-api';
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  computed,
+} from '@vue/composition-api';
 import sl from '@/serviceLocator';
-import { PersonInfo } from '@oare/types';
+import { PersonInfo, PersonRoleResponse } from '@oare/types';
 
 export default defineComponent({
   props: {
@@ -119,8 +130,10 @@ export default defineComponent({
       husbands: [],
       siblings: [],
       children: [],
-      durableRoles: [],
       discussion: [],
+    });
+    const personRoles = ref<PersonRoleResponse>({
+      durableRoles: [],
       temporaryRoles: [],
     });
 
@@ -128,6 +141,7 @@ export default defineComponent({
       try {
         loading.value = true;
         person.value = await server.getPersonInfo(props.uuid);
+        personRoles.value = await server.getPersonRoles(props.uuid);
       } catch (err) {
         actions.showErrorSnackbar(
           'Error retrieving person info. Please try again.',
@@ -138,9 +152,26 @@ export default defineComponent({
       }
     });
 
+    const personHasData = computed(() => {
+      return (
+        person.value.father ||
+        person.value.mother ||
+        person.value.husbands.length > 0 ||
+        person.value.asshatumWives.length > 0 ||
+        person.value.amtumWives.length > 0 ||
+        person.value.siblings.length > 0 ||
+        person.value.children.length > 0 ||
+        person.value.discussion.length > 0 ||
+        personRoles.value.durableRoles.length > 0 ||
+        personRoles.value.temporaryRoles.length > 0
+      );
+    });
+
     return {
       loading,
       person,
+      personRoles,
+      personHasData,
     };
   },
 });
