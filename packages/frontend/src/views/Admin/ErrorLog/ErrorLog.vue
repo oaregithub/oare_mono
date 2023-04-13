9e2162d8803edb04be9b5cc3fd2c0deb0e5ef1cf
hbludworth <63749412+hbludworth@users.noreply.github.com>
2023-04-13 14:53:44 -0600

    updated error log page (#1677)
    * updated error log page
    
    * updates list on indiv status change

--- packages/frontend/src/views/Admin/ErrorLog/ErrorLog.vue
+++ packages/frontend/src/views/Admin/ErrorLog/ErrorLog.vue
@@ -154,10 +154,10 @@ export default defineComponent({
     const serverCount = ref(0);
 
     const page = useQueryParam('page', '1', false);
-    const limit = useQueryParam('rows', '10', true);
+    const limit = useQueryParam('rows', '25', true);
     const sort = useQueryParam('sort', 'timestamp', true);
     const desc = useQueryParam('desc', 'true', true);
-    const status = useQueryParam('status', '', true);
+    const status = useQueryParam('status', 'New', true);
     const user = useQueryParam('user', '', true);
     const description = useQueryParam('description', '', true);
     const stacktrace = useQueryParam('stacktrace', '', true);
@@ -227,8 +227,9 @@ export default defineComponent({
       showErrorDetails.value = true;
     };
 
-    const updateStatus = (status: ErrorStatus) => {
+    const updateStatus = async (status: ErrorStatus) => {
       dialogError.value.status = status;
+      await getErrorLog();
     };
 
     const updateMultipleStatus = async (status: ErrorStatus) => {
@@ -237,16 +238,7 @@ export default defineComponent({
           error => error.uuid
         );
         await server.updateErrorStatus(selectedErrorUuids, status);
-        errorList.value = errorList.value.map(error => {
-          if (selectedErrors.value.includes(error)) {
-            return {
-              ...error,
-              status,
-            };
-          } else {
-            return error;
-          }
-        });
+        await getErrorLog();
         selectedErrors.value = [];
         await resetAdminBadge();
       } catch (err) {
