b8db396bce9c0e26657c6ec915c9f8fb639f14ce
Harrison Bludworth <harrisonbludworth@gmail.com>
2023-05-17 16:48:51 -0600

    docs and cleans asset dao

--- packages/backend/src/daos/AssetDao/index.ts
+++ packages/backend/src/daos/AssetDao/index.ts
@@ -2,12 +2,21 @@ import { LinkItem } from '@oare/types';
 import { Knex } from 'knex';
 import knex from '@/connection';
 
+// VERIFIED COMPLETE
+
 class AssetDao {
+  /**
+   * Searches for assets by name or UUID. Used for autocomplete when connecting link properties.
+   * @param search The search string. Could be a UUID or a name.
+   * @param trx Knex Transaction. Optional.
+   * @returns Array of matching, ordered `LinkItem` objects.
+   */
   async searchAssets(
     search: string,
     trx?: Knex.Transaction
   ): Promise<LinkItem[]> {
     const k = trx || knex;
+
     const rows: LinkItem[] = await k('asset')
       .innerJoin('alias', 'alias.reference_uuid', 'asset.uuid')
       .select('asset.uuid as objectUuid', 'alias.name as objectDisplay')
@@ -22,4 +31,7 @@ class AssetDao {
   }
 }
 
+/**
+ * AssetDao instance as a singleton
+ */
 export default new AssetDao();
